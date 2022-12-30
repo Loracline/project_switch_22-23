38b8c6d344a47580d0c2c3cb6f732a15268bb398
jussaragermano <1221845@isep.ipp.pt>
2022-12-30 14:57:55 +0000

    US001 : Development : Refinement of class profile and refined respective tests;

--- src/main/java/org/switch2022/project/Profile.java
+++ src/main/java/org/switch2022/project/Profile.java
@@ -2,18 +2,27 @@ package org.switch2022.project;
 
 import java.util.Objects;
 
+/**
+ * Class Profile is built to create and manage new profiles.
+ * A profile is defined by a name.
+ */
+
 public class Profile {
     private String profileName;
 
-    Profile(String profileName) {
-        this.profileName = profileName;
-    }
 
+    /**
+     * Constructs a default User profile
+     */
+    public Profile(String name) {
+        this.profileName = name.toLowerCase();
+    }
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Profile)) return false;
-        Profile profile = (Profile) o;
-        return Objects.equals(profileName, profile.profileName);
+        Profile profile1 = (Profile) o;
+        return Objects.equals(profileName, profile1.profileName.toLowerCase());
     }
 }
+
--- src/test/java/org/switch2022/project/ProfileTest.java
+++ src/test/java/org/switch2022/project/ProfileTest.java
@@ -1,7 +1,39 @@
 package org.switch2022.project;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProfileTest {
 
+    @Test
+    void createProfileSucessfully() {
+        String profileName = "Administrator";
+        Profile profile = new Profile(profileName);
+    }
+    @Test
+    void ensureSameObjectEqualsItself() {
+        Profile reference = new Profile("Admin");
+        Profile other = reference;
+        boolean expected = true;
+        boolean result = reference.equals(other);
+        assertEquals(expected, result);
+    }
+    @Test
+    void ensureTwoArraysAreNotTheSame() {
+        Profile reference = new Profile("User");
+        Profile other = new Profile("Manager");
+        boolean expected = false;
+        boolean result = reference.equals(other);
+        assertEquals(expected, result);
+    }
+    @Test
+    void ensureObjectDoesNotEqualsOtherTypeOfObject() {
+        Profile reference = new Profile("User");
+        String other = new String("User");
+        boolean expected = false;
+        boolean result = reference.equals(other);
+        assertEquals(expected, result);
+    }
 }
+
