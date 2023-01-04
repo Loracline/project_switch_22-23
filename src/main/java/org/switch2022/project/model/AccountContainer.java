7d55cc7bf960c8bf0e99e0b8fd87becd707e5a9a
Margarida Ferreira <1221847@isep.ipp.pt>
2023-01-04 09:21:03 +0000

    Corrected typo in AccountContainer class

--- src/main/java/org/switch2022/project/model/AccountContainer.java
+++ src/main/java/org/switch2022/project/model/AccountContainer.java
@@ -3,18 +3,18 @@ package org.switch2022.project.model;
 import java.util.List;
 import java.util.Objects;
 
-public class AccountCountainer {
+public class AccountContainer {
   private List<Account> accountList;
 
-  public AccountCountainer(List<Account> accountList){
+  public AccountContainer(List<Account> accountList){
     this.accountList = accountList;
   }
 
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
-    if (!(o instanceof AccountCountainer)) return false;
-    AccountCountainer that = (AccountCountainer) o;
+    if (!(o instanceof AccountContainer)) return false;
+    AccountContainer that = (AccountContainer) o;
     return Objects.equals(accountList, that.accountList);
   }
 }
