b421e3b21a3ae867d29671f219c939c90a1cf83b
Margarida Ferreira <1221847@isep.ipp.pt>
2023-01-05 13:08:17 +0000

    US002: Development: Refinement of AccountContainer and RegisterAccountController classes; refinement of RegisterAccountControllerTest class

--- src/main/java/org/switch2022/project/model/AccountContainer.java
+++ src/main/java/org/switch2022/project/model/AccountContainer.java
@@ -30,6 +30,10 @@ public class AccountContainer {
     return Objects.equals(accounts, that.accounts);
   }
 
+  public List<Account> getAccounts(){
+    return this.accounts;
+  }
+
   public Account getAccountByEmail(String email) {
     String accountEmail = email;
     Account requestedAccount = null;
@@ -40,5 +44,40 @@ public class AccountContainer {
       }
     }
     return requestedAccount;
+  }
+
+  /**
+   * This method checks if there is any account in the list that has the
+   * e-mail passed by parameter.
+   *
+   * @param email that one must find out if exists in the Repository
+   * @return TRUE if there is an account with given e-mail and FALSE otherwise
+   */
+
+  public boolean doesEmailExist(String email) {
+    boolean emailExists = false;
+    for (int i = 0; i < this.accounts.size(); i++) {
+      if (accounts.get(i).getEmail().equals(email)) {
+        emailExists = true;
+        break;
+      }
+    }
+    return emailExists;
+  }
+
+  /**
+   * Add account method to save registered accounts in the attribute ACCOUNTS LIST.
+   * It adds an account if the email doesn't exist in the accounts list.
+   *
+   * @param acc instance of Account to be added to accountList
+   */
+  public boolean addAccount(Account acc) {
+    boolean isAccountAdded = false;
+    String email = acc.getEmail();
+    if (!doesEmailExist(email)) {
+      this.accounts.add(acc);
+      isAccountAdded = true;
+    }
+    return isAccountAdded;
   }
 }
