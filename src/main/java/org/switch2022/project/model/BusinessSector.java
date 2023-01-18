37cd6435652f643c6091b206c0d0a53d098370b4
Cristiana Moreira [1221826] <1221826@isep.ipp.pt>
2023-01-18 14:18:12 +0000

    US 015: creation of BusinessSector class and tests

--- src/main/java/org/switch2022/project/model/BusinessSector.java
+++ src/main/java/org/switch2022/project/model/BusinessSector.java
@@ -1,9 +1,19 @@
 package org.switch2022.project.model;
 
+import java.util.Objects;
+
 public class BusinessSector {
     private String businessSectorName;
 
     public BusinessSector(String businessSectorName) {
-        this.businessSectorName = businessSectorName;
+        this.businessSectorName = businessSectorName.toLowerCase();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof BusinessSector)) return false;
+        BusinessSector that = (BusinessSector) o;
+        return Objects.equals(businessSectorName, that.businessSectorName.toLowerCase());
     }
 }
