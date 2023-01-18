d7d71d1393a9419434b3189f20e0579230c1b6bf
Caroline Dantas Mendes da Silva [1221823] <1221823@isep.ipp.pt>
2023-01-18 17:13:29 +0000

    US007: Development: creation of class ProjectTypologyContainer, ProjectTypologyController and ProjectTypology. Update of class ProjectTypology and ProjectTypologyContainer.

--- src/main/java/org/switch2022/project/container/ProjectTypologyContainer.java
+++ src/main/java/org/switch2022/project/container/ProjectTypologyContainer.java
@@ -1,4 +1,30 @@
 package org.switch2022.project.container;
 
+import org.switch2022.project.model.ProjectTypology;
+import java.util.List;
+
 public class ProjectTypologyContainer {
+
+    private List<ProjectTypology> typologies;
+
+    public ProjectTypologyContainer(List<ProjectTypology> typologies) {
+        this.typologies = typologies;
+    }
+
+    public boolean doesTypologyExist(ProjectTypology projectTypology) {
+        return this.typologies.contains(projectTypology);
+    }
+
+    public boolean createProjectTypology(String projectTypology) {
+        ProjectTypology newprojectTypology = new ProjectTypology(projectTypology);
+        boolean isAddedToList = false;
+        if (!doesTypologyExist(newprojectTypology)) {
+            typologies.add(newprojectTypology);
+            isAddedToList = true;
+        }
+        return isAddedToList;
+    }
+    public List<ProjectTypology> getTypologies() {
+        return typologies;
+    }
 }
---
+++ src/test/java/org/switch2022/project/container/ProjectTypologyContainerTest.java
@@ -0,0 +1,7 @@
+package org.switch2022.project.container;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProjectTypologyContainerTest {
+
+}
