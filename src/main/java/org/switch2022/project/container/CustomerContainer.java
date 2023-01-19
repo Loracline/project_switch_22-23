0c0fa0b4ac581f2a33cf11f8e74d698a80111306
Jussara Oliveira [1221845] <1221845@isep.ipp.pt>
2023-01-19 20:23:32 +0000

    US009: Development: add doesCustomerExist,addCustomer and getCustomerByName methods in CustomerContainer and respective tests in CustomerContainerTest.

--- src/main/java/org/switch2022/project/container/CustomerContainer.java
+++ src/main/java/org/switch2022/project/container/CustomerContainer.java
@@ -46,4 +46,62 @@ public class CustomerContainer {
     }
     return requestedCustomer;
   }
+
+  /**
+   * Check if first is lower than second
+   *
+   * @param first  integer
+   * @param second integer
+   * @return True if first is lower than second, and False otherwise
+   */
+  public static boolean isLower(int first, int second) {
+    return first < second;
+  }
+
+  /**
+   * This method validates if customer exits
+   *
+   * @param customerName one must check
+   * @return true if customer exists in customers
+   */
+  private boolean doesCustomerExist(Customer customerName) {
+    return this.customers.contains(customerName);
+  }
+
+  /**
+   * This method creates customer and adds it to customers
+   *
+   * @param customerName one must add
+   */
+
+  public boolean addCustomer(String customerName) {
+    Customer newCustomer = new Customer(customerName);
+    boolean isAddedToList = false;
+    if (customerName != null && !customerName.isEmpty() && !doesCustomerExist(newCustomer)) {
+      customers.add(new Customer(customerName));
+      isAddedToList = true;
+    }
+    return isAddedToList;
+  }
+
+  /**
+   * This method identifies the requested customer by indication of customerName
+   *
+   * @return an object Customer
+   */
+
+  public Customer getCustomerByName(String customerName) {
+    Customer customer = new Customer(customerName);
+    Customer requestedCustomer = null;
+    int i = 0;
+    while (isLower(i, this.customers.size())) {
+      if (this.customers.contains(customer)) {
+        requestedCustomer = customers.get(i);
+        break;
+      }
+      i++;
+    }
+    return requestedCustomer;
+  }
+
 }
