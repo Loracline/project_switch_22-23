dac442c0a510255f1f61e0811a90532a8bd2fe3f
Bárbara Oliveira [1221820] <1221820@isep.ipp.pt>
2023-01-23 11:48:19 +0000

    US013 : Development : Testing the implementation of the US by testing the Controller Class.

--- src/test/java/org/switch2022/project/controller/AddUserToProjectControllerTest.java
+++ src/test/java/org/switch2022/project/controller/AddUserToProjectControllerTest.java
@@ -13,53 +13,70 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+/**
+ * This class tests the implementation of these three US:
+ * US011 - As Manager, I want to associate a user as Team Member of a project.
+ * US012 - As Manager, I want to define the PO of a project.
+ * US013 - As Manager, I want to define the SM of a project.
+ */
 class AddUserToProjectControllerTest {
-
     /**
-     * BeforeEach and AfterEach executes common code before/after running the tests below.
+     * BeforeEach and AfterEach executes common code before/after running the
+     * tests below.
      */
 
     //accounts
     Account accountOne, accountTwo, accountThree;
     List<Account> accounts;
     AccountContainer accountContainer;
+
     //profiles
     Profile profileOne, profileTwo, profileThree;
     List<Profile> profiles;
     ProfileContainer profileContainer;
+
     //customers
     Customer customerOne, customerTwo;
     List<Customer> customers;
     CustomerContainer customerContainer;
+
     //projects
     Project projectOne, projectTwo;
     List<Project> projects;
     ProjectContainer projectContainer;
+
     //project typologies
     ProjectTypology projectTypologyOne, projectTypologyTwo;
     List<ProjectTypology> typologies;
     ProjectTypologyContainer projectTypologyContainer;
+
     //business sectors
     BusinessSector businessSector;
     List<BusinessSector> businessSectors;
     BusinessSectorContainer businessSectorContainer;
-    //account in project
+
+    //accounts in project
     LocalDate startDate;
-    AccountInProject accountInProject1, accountInProject2;
+    AccountInProject accountInProject1, accountInProject2, accountInProject3;
     List<AccountInProject> accountsInProject;
     AccountInProjectContainer accountInProjectContainer;
+
     //company
     Company company;
-
 
     @BeforeEach
     void setUp() {
         //accounts
-        accountOne = new Account("Mike", "mike@isep.ipp.pt", 932755689, null);
-        accountTwo = new Account("Emma", "emma@isep.ipp.pt", 932755688, null);
-        accountThree = new Account("Jane", "jane@isep.ipp.pt", 932755687, null);
+        accountOne = new Account("Mike", "mike@isep.ipp.pt",
+                932755689, null);
+        accountTwo = new Account("Emma", "emma@isep.ipp.pt",
+                932755688, null);
+        accountThree = new Account("Jane", "jane@isep.ipp.pt",
+                932755687, null);
+
         accounts = new ArrayList<>();
         accountContainer = new AccountContainer(accounts);
         accounts.add(accountOne);
@@ -70,6 +87,7 @@ class AddUserToProjectControllerTest {
         profileOne = new Profile("Administrator");
         profileTwo = new Profile("User");
         profileThree = new Profile("Manager");
+
         profiles = new ArrayList<>();
         profileContainer = new ProfileContainer(profiles);
         profiles.add(profileOne);
@@ -77,14 +95,16 @@ class AddUserToProjectControllerTest {
 
         //business sectors
         businessSector = new BusinessSector("fishing");
+
         businessSectors = new ArrayList<>();
         businessSectorContainer = new BusinessSectorContainer(businessSectors);
         businessSectors.add(businessSector);
 
-
         //project typologies
         projectTypologyOne = new ProjectTypology("Fixed Cost");
-        projectTypologyTwo = new ProjectTypology("Fixed time and materials");
+        projectTypologyTwo = new ProjectTypology(
+                "Fixed time and materials");
+
         typologies = new ArrayList<>();
         projectTypologyContainer = new ProjectTypologyContainer(typologies);
         typologies.add(projectTypologyOne);
@@ -100,13 +120,12 @@ class AddUserToProjectControllerTest {
         customers.add(customerTwo);
 
         //projects
-        projectOne = new Project("proj001", "software development management",
-                customerOne,
-                projectTypologyOne,
-                businessSector);
-        projectTwo = new Project("proj002", "project software", customerTwo,
-                projectTypologyTwo,
-                businessSector);
+        projectOne = new Project("proj001",
+                "software development management", customerOne,
+                projectTypologyOne, businessSector);
+        projectTwo = new Project("proj002", "project software",
+                customerTwo, projectTypologyTwo, businessSector);
+
         projects = new ArrayList<>();
         projects.add(projectOne);
         projects.add(projectTwo);
@@ -117,10 +136,13 @@ class AddUserToProjectControllerTest {
                 7.5f, 40.0f, startDate);
         accountInProject2 = new AccountInProject(accountTwo, projectTwo, "Product Owner",
                 7.5f, 40.0f, startDate);
+        accountInProject3 = new AccountInProject(accountThree, projectOne, "Scrum Master",
+                7.5f, 40.0f, startDate);
 
         accountsInProject = new ArrayList<>();
         accountsInProject.add(accountInProject1);
         accountsInProject.add(accountInProject2);
+        accountsInProject.add(accountInProject3);
         accountInProjectContainer = new AccountInProjectContainer(accountsInProject);
 
         //company
@@ -137,40 +159,51 @@ class AddUserToProjectControllerTest {
         accountThree = null;
         accounts.clear();
         accountContainer = null;
+
         //profiles
         profileOne = null;
         profileTwo = null;
         profileThree = null;
         profiles.clear();
         profileContainer = null;
+
         //business sectors
         businessSector = null;
         businessSectors.clear();
         businessSectorContainer = null;
+
         //project typologies
         projectTypologyOne = null;
         projectTypologyTwo = null;
         typologies.clear();
         projectTypologyContainer = null;
+
         //customers
         customerOne = null;
         customerTwo = null;
         customers.clear();
         customerContainer = null;
+
         //projects
         projectOne = null;
         projectTwo = null;
         projects.clear();
         projectContainer = null;
+
         //accounts in project
         accountInProject1 = null;
         accountInProject2 = null;
+        accountInProject3 = null;
         accountsInProject.clear();
         accountInProjectContainer = null;
+
         //company
         company = null;
     }
 
+    /**
+     * US012 - As Manager, I want to define the PO of a project.
+     */
     @Test
     void ensureProductOwnerIsSuccessfullyAssociatedToAProject() {
         //Arrange
@@ -181,7 +214,7 @@ class AddUserToProjectControllerTest {
         accountDTO.phoneNumber = 912345678;
         accountDTO.photo = null;
         //projectDTO
-        ProjectDTO projectDTO = new ProjectDTO("id001","Test",customerOne,
+        ProjectDTO projectDTO = new ProjectDTO("id001", "Test", customerOne,
                 projectTypologyOne
                 , businessSector);
 
@@ -195,8 +228,8 @@ class AddUserToProjectControllerTest {
         accountInProjectDTOPO.role = "Product Owner";
         accountInProjectDTOPO.costPerHour = 7.5f;
         accountInProjectDTOPO.percentageAllocation = 45.0f;
-        accountInProjectDTOPO.startDate = LocalDate.of(2023, 01, 19);
-        accountInProjectDTOPO.endDate = LocalDate.of(2023, 01, 22);
+        accountInProjectDTOPO.startDate = LocalDate.of(2023, 1, 19);
+        accountInProjectDTOPO.endDate = LocalDate.of(2023, 1, 22);
         AddUserToProjectController controller = new AddUserToProjectController(company);
 
         //Act
@@ -207,6 +240,9 @@ class AddUserToProjectControllerTest {
 
     }
 
+    /**
+     * US011 - As Manager, I want to associate a user as Team Member of a project.
+     */
     @Test
     void ensureTeamMemberIsSuccessfullyAssociatedToAProject() {
         //Arrange
@@ -217,7 +253,7 @@ class AddUserToProjectControllerTest {
         accountDTO.phoneNumber = 912345688;
         accountDTO.photo = null;
         //projectDTO
-        ProjectDTO projectDTO = new ProjectDTO("id001","Test",customerOne,
+        ProjectDTO projectDTO = new ProjectDTO("id001", "Test", customerOne,
                 projectTypologyOne
                 , businessSector);
 
@@ -231,8 +267,8 @@ class AddUserToProjectControllerTest {
         accountInProjectDTOTM.role = "Team Member";
         accountInProjectDTOTM.costPerHour = 7.5f;
         accountInProjectDTOTM.percentageAllocation = 45.0f;
-        accountInProjectDTOTM.startDate = LocalDate.of(2023, 01, 19);
-        accountInProjectDTOTM.endDate = LocalDate.of(2023, 01, 22);
+        accountInProjectDTOTM.startDate = LocalDate.of(2023, 1, 19);
+        accountInProjectDTOTM.endDate = LocalDate.of(2023, 1, 22);
         AddUserToProjectController controller = new AddUserToProjectController(company);
 
         //Act
@@ -242,4 +278,76 @@ class AddUserToProjectControllerTest {
         assertTrue(result);
     }
 
+    /**
+     * US013 - As Manager, I want to define the SM of a project.
+     */
+    @Test
+    void ensureScrumMasterIsSuccessfullyAssociatedToAProject() {
+        //Arrange
+        //accountDTO
+        AccountDTO accountDTO = new AccountDTO();
+        accountDTO.name = "Mary";
+        accountDTO.email = "mary@isep.ipp.pt";
+        accountDTO.phoneNumber = 852456951;
+        accountDTO.photo = null;
+        //projectDTO
+        ProjectDTO projectDTO = new ProjectDTO("id001", "Test", customerOne,
+                projectTypologyOne
+                , businessSector);
+
+        projectDTO.name = "Test";
+        projectDTO.status = "planned";
+        projectDTO.businessSector = new BusinessSector("IT Sector");
+        //account in project dto - scrum master
+        AccountInProjectDTO accountInProjectDTOSM = new AccountInProjectDTO();
+        accountInProjectDTOSM.accountDTO = accountDTO;
+        accountInProjectDTOSM.projectDTO = projectDTO;
+        accountInProjectDTOSM.role = "Scrum Master";
+        accountInProjectDTOSM.costPerHour = 7.5f;
+        accountInProjectDTOSM.percentageAllocation = 45.0f;
+        accountInProjectDTOSM.startDate = LocalDate.of(2023, 1, 19);
+        accountInProjectDTOSM.endDate = LocalDate.of(2023, 1, 22);
+        AddUserToProjectController controller = new AddUserToProjectController(company);
+
+        //Act
+        boolean result = controller.addUserToProject(accountInProjectDTOSM);
+
+        //Assert
+        assertTrue(result);
+    }
+
+    @Test
+    void ensureAllocationFailsBecauseRoleIsNotValid() {
+        //Arrange
+        //accountDTO
+        AccountDTO accountDTO = new AccountDTO();
+        accountDTO.name = "Peter";
+        accountDTO.email = "peter@isep.ipp.pt";
+        accountDTO.phoneNumber = 564897231;
+        accountDTO.photo = null;
+        //projectDTO
+        ProjectDTO projectDTO = new ProjectDTO("id001", "Test", customerOne,
+                projectTypologyOne
+                , businessSector);
+
+        projectDTO.name = "Test";
+        projectDTO.status = "planned";
+        projectDTO.businessSector = new BusinessSector("IT Sector");
+        //account in project dto - scrum master
+        AccountInProjectDTO accountInProjectDTOSM = new AccountInProjectDTO();
+        accountInProjectDTOSM.accountDTO = accountDTO;
+        accountInProjectDTOSM.projectDTO = projectDTO;
+        accountInProjectDTOSM.role = "Administrator";
+        accountInProjectDTOSM.costPerHour = 7.5f;
+        accountInProjectDTOSM.percentageAllocation = 45.0f;
+        accountInProjectDTOSM.startDate = LocalDate.of(2023, 1, 19);
+        accountInProjectDTOSM.endDate = LocalDate.of(2023, 1, 22);
+        AddUserToProjectController controller = new AddUserToProjectController(company);
+
+        //Act
+        boolean result = controller.addUserToProject(accountInProjectDTOSM);
+
+        //Assert
+        assertFalse(result);
+    }
 }
