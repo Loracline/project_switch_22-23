294ee640c62c6bdd5b528bff782952c706787476
Jussara Saraiva Germano de Oliveira [1221845] <1221845@isep.ipp.pt>
2023-01-24 16:19:10 +0000

    US009: Development: Refined addCustomer of AddCustomerController.

--- src/main/java/org/switch2022/project/controller/AddCustomerController.java
+++ src/main/java/org/switch2022/project/controller/AddCustomerController.java
@@ -21,17 +21,14 @@ public class AddCustomerController {
      * Method addCustomer
      *
      * @return true if customer is added
-     * @return false if customer isn't added successfully
+     *
      */
 
     public boolean addCustomer(String customerName, String email) {
         boolean addCustomer = false;
         if (company.validateAdministrator(email)) {
-            if (company.addCustomer(customerName)) {
-                addCustomer = true;
-            }
+            addCustomer = company.addCustomer(customerName);
         }
-
         return addCustomer;
     }
 }
