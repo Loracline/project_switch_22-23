0881e6c9326b85769208ba21d0f5245079b01319
Sofia Oliveira 1221862 <1221862@isep.ipp.pt>
2023-01-26 17:36:20 +0000

    US015: Development: updated the method on class ListOfProjectsMapper to static and split the method in two in order to permit code modularization. Created class ListOfProjectsMapperTest and the respective test. Added missing javadoc.

--- src/main/java/org/switch2022/project/utils/mapper/ListOfProjectsMapper.java
+++ src/main/java/org/switch2022/project/utils/mapper/ListOfProjectsMapper.java
@@ -7,15 +7,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ListOfProjectsMapper {
+    /**
+     * Constructor of the class ListOfProjectsMapper.
+     */
+    private ListOfProjectsMapper() {
+    }
 
-    public List<ListProjectsDTO> toDTO(List<Project> projects) {
+    /**
+     * This method receives a project and maps/transforms the project
+     * into DTO
+     */
+    private static ListProjectsDTO getDTOFromProject(Project project) {
+        return new ListProjectsDTO(project.getProjectCode(),
+                project.getProjectName(), project.getCustomer().getCustomerName(),
+                project.getProjectStatus(),
+                project.getProjectTypology().getProjectTypologyName(),
+                project.getBusinessSector().getBusinessSectorName());
+    }
+
+    /**
+     * This method receives a list of projects, creates an empty list, maps/transforms the
+     * projects
+     * into DTOs and then stores them in a list to be returned by the method developed in
+     * GetListOfProjectsController
+     */
+    public static List<ListProjectsDTO> getListOfProjectsDTO(List<Project> projects) {
         List<ListProjectsDTO> listOfProjectsDto = new ArrayList<>();
         for (Project project : projects) {
-            ListProjectsDTO listProjectsDTO = new ListProjectsDTO(project.getProjectCode(),
-                    project.getProjectName(), project.getCustomer().getCustomerName(),
-                    project.getProjectStatus(),
-                    project.getProjectTypology().getProjectTypologyName(),
-                    project.getBusinessSector().getBusinessSectorName());
+            ListProjectsDTO listProjectsDTO = getDTOFromProject(project);
 
             listOfProjectsDto.add(listProjectsDTO);
         }
