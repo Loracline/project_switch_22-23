6395ff7ffdbdb7670dcf932a165686cf91c280cf
Sofia Oliveira 1221862 <1221862@isep.ipp.pt>
2023-02-26 21:35:40 +0000

    NO US: Development: Code reformatting in order to decreased code smells pointed out in sonarqube

--- src/main/java/org/switch2022/project/container/ProjectContainer.java
+++ src/main/java/org/switch2022/project/container/ProjectContainer.java
@@ -1,8 +1,8 @@
 package org.switch2022.project.container;
 
-import org.switch2022.project.model.Project;
 import org.switch2022.project.dto.ProjectDtoAsManager;
 import org.switch2022.project.dto.mapper.ProjectMapper;
+import org.switch2022.project.model.Project;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,69 +12,69 @@ import java.util.List;
  * of this company.
  */
 public class ProjectContainer {
-  /**
-   * Attributes
-   */
-  private final List<Project> projects = new ArrayList<>();
+    /**
+     * Attributes
+     */
+    private final List<Project> projects = new ArrayList<>();
 
-  /**
-   * This method verify the existence of a project by code confirmation.
-   *
-   * @return the project with given code.
-   */
-  public Project getProjectByCode(String code) {
-    Project projectRequested = null;
-    int i = 0;
-    while (i < this.projects.size() && (projectRequested != (projects.get(i)))) {
-      if (projects.get(i).getProjectCode().equalsIgnoreCase(code)) {
-        projectRequested = projects.get(i);
-      }
-      i++;
+    /**
+     * This method verify the existence of a project by code confirmation.
+     *
+     * @return the project with given code.
+     */
+    public Project getProjectByCode(String code) {
+        Project projectRequested = null;
+        int i = 0;
+        while (i < this.projects.size() && (projectRequested != (projects.get(i)))) {
+            if (projects.get(i).getProjectCode().equalsIgnoreCase(code)) {
+                projectRequested = projects.get(i);
+            }
+            i++;
+        }
+        return projectRequested;
     }
-    return projectRequested;
-  }
 
-  /**
-   * Getter method for the attribute: projects.
-   *
-   * @return a list of all the projects in the container.
-   */
-  public List<Project> getProjects() {
-    return projects;
-  }
-
-  /**
-   * This method creates a new project and adds it to the container if it
-   * doesn't already exist.
-   *
-   * @param projectDTOAsManager data transfer object of the attributes of project.
-   * @return TRUE if registered and FALSE otherwise.
-   */
-  public boolean registerProject(ProjectDtoAsManager projectDTOAsManager) {
-    Project project = ProjectMapper.getProjectFromDTO(projectDTOAsManager);
-    boolean projectRegistered = false;
-    if (!doesProjectExist(project)) {
-      projects.add(project);
-      projectRegistered = true;
+    /**
+     * Getter method for the attribute: projects.
+     *
+     * @return a list of all the projects in the container.
+     */
+    public List<Project> getProjects() {
+        return projects;
     }
-    return projectRegistered;
-  }
 
-  /**
-   * This method verifies if the project already exists in the container through the project code.
-   *
-   * @param project existence that shall be verified.
-   * @return TRUE if exists, and FALSE otherwise.
-   */
-  private boolean doesProjectExist(Project project) {
-    boolean projectExistence = false;
-    int i = 0;
-    while (i < projects.size() && !projectExistence) {
-      if (projects.get(i).getProjectCode().equals(project.getProjectCode())) {
-        projectExistence = true;
-      }
-      i++;
+    /**
+     * This method creates a new project and adds it to the container if it
+     * doesn't already exist.
+     *
+     * @param projectDTOAsManager data transfer object of the attributes of project.
+     * @return TRUE if registered and FALSE otherwise.
+     */
+    public boolean registerProject(ProjectDtoAsManager projectDTOAsManager) {
+        Project project = ProjectMapper.getProjectFromDTO(projectDTOAsManager);
+        boolean projectRegistered = false;
+        if (!doesProjectExist(project)) {
+            projects.add(project);
+            projectRegistered = true;
+        }
+        return projectRegistered;
     }
-    return projectExistence;
-  }
+
+    /**
+     * This method verifies if the project already exists in the container through the project code.
+     *
+     * @param project existence that shall be verified.
+     * @return TRUE if exists, and FALSE otherwise.
+     */
+    private boolean doesProjectExist(Project project) {
+        boolean projectExistence = false;
+        int i = 0;
+        while (i < projects.size() && !projectExistence) {
+            if (projects.get(i).getProjectCode().equals(project.getProjectCode())) {
+                projectExistence = true;
+            }
+            i++;
+        }
+        return projectExistence;
+    }
 }
