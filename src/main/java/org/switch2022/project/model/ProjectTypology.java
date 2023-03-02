b8a7f5fdc80b9169271d5d4cbf6d336b4736169b
João Serra [1221842] <1221842@isep.ipp.pt>
2023-03-02 17:20:39 +0000

    US 010 : Development : Correction of some classes, diagrams and subsequence consequences.

--- src/main/java/org/switch2022/project/model/ProjectTypology.java
+++ src/main/java/org/switch2022/project/model/ProjectTypology.java
@@ -7,57 +7,53 @@ import java.util.Objects;
  * A project typology is defined by name.
  */
 public class ProjectTypology {
-    /**
-     * Attributes
-     */
-    private final String projectTypologyName;
+  /**
+   * Attributes
+   */
+  private final String projectTypologyName;
 
-    /**
-     * Constructor
-     */
-    public ProjectTypology(String projectTypologyName) {
-        this.projectTypologyName = projectTypologyName.toLowerCase();
-    }
+  /**
+   * Constructor
+   */
+  public ProjectTypology(String projectTypologyName) {
+    this.projectTypologyName = projectTypologyName.toLowerCase();
+  }
 
-    public String getProjectTypologyName() {
-        return this.projectTypologyName;
-    }
+  public String getProjectTypologyName() {
+    return this.projectTypologyName;
+  }
 
-    public boolean checkProjectTypologyFromName(String projectTypologyName) {
-        return this.projectTypologyName.equals(projectTypologyName);
+  /**
+   * This method checks if two instances of ProjectTypology are equal by
+   * comparing its names.
+   *
+   * @param toCompare ProjectTypology instance to compare with.
+   * @return TRUE if the two have the same typology name, and FALSE otherwise.
+   */
+  @Override
+  public boolean equals(Object toCompare) {
+    if (this == toCompare) {
+      return true;
     }
+    if (toCompare == null) {
+      return false;
+    }
+    if (toCompare.getClass() != this.getClass()) {
+      return false;
+    }
+    ProjectTypology that = (ProjectTypology) toCompare;
+    return Objects.equals(projectTypologyName,
+            that.projectTypologyName.toLowerCase());
+  }
 
-    /**
-     * This method checks if two instances of ProjectTypology are equal by
-     * comparing its names.
-     *
-     * @param toCompare ProjectTypology instance to compare with.
-     * @return TRUE if the two have the same typology name, and FALSE otherwise.
-     */
-    @Override
-    public boolean equals(Object toCompare) {
-        if (this == toCompare) {
-            return true;
-        }
-        if (toCompare == null) {
-            return false;
-        }
-        if (toCompare.getClass() != this.getClass()) {
-            return false;
-        }
-        ProjectTypology that = (ProjectTypology) toCompare;
-        return Objects.equals(projectTypologyName,
-                that.projectTypologyName.toLowerCase());
-    }
-
-    /**
-     * The hashCode() method is used to generate a unique hash code for an
-     * object, based on the object's state.
-     *
-     * @return a unique value that represents the object.
-     */
-    @Override
-    public int hashCode() {
-        return Objects.hash(projectTypologyName);
-    }
+  /**
+   * The hashCode() method is used to generate a unique hash code for an
+   * object, based on the object's state.
+   *
+   * @return a unique value that represents the object.
+   */
+  @Override
+  public int hashCode() {
+    return Objects.hash(projectTypologyName);
+  }
 }
--- src/test/java/org/switch2022/project/controller/GetListOfProjectsControllerTest.java
+++ src/test/java/org/switch2022/project/controller/GetListOfProjectsControllerTest.java
@@ -4,9 +4,9 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.switch2022.project.container.*;
-import org.switch2022.project.model.*;
 import org.switch2022.project.dto.ManagerListProjectsDto;
 import org.switch2022.project.dto.ProjectDtoAsManager;
+import org.switch2022.project.model.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,121 +15,123 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GetListOfProjectsControllerTest {
 
-    Account accountOne, accountManager;
-    Customer customer;
-    ProjectTypology projectTypology;
-    BusinessSector businessSector;
-    Project projectOne, projectTwo;
-    AccountContainer accountContainer;
-    ProfileContainer profileContainer;
-    ProjectContainer projectContainer;
-    ProjectTypologyContainer projectTypologyContainer;
-    BusinessSectorContainer businessSectorContainer;
-    CustomerContainer customerContainer;
-    Company company;
-    GetListOfProjectsController getListOfProjectscontroller;
+  Account accountOne, accountManager;
+  Customer customer;
+  ProjectTypology projectTypology;
+  BusinessSector businessSector;
+  Project projectOne, projectTwo;
+  AccountContainer accountContainer;
+  ProfileContainer profileContainer;
+  ProjectContainer projectContainer;
+  ProjectTypologyContainer projectTypologyContainer;
+  BusinessSectorContainer businessSectorContainer;
+  CustomerContainer customerContainer;
+  Company company;
+  GetListOfProjectsController getListOfProjectscontroller;
 
-    @BeforeEach
-    void setUp() {
-        //accounts
-        accountOne = new Account("Paul", "paul@isep.ipp.pt", 939855689, null);
-        accountManager = new Account("Mike", "mike@isep.ipp.pt", 932755689, null);
+  @BeforeEach
+  void setUp() {
+    //accounts
+    accountOne = new Account("Paul", "paul@isep.ipp.pt", 939855689, null);
+    accountManager = new Account("Mike", "mike@isep.ipp.pt", 932755689, null);
 
-        //account container
-        accountContainer = new AccountContainer();
-        accountContainer.addAccount("Paul", "paul@isep.ipp.pt", 939855689, null);
-        accountContainer.addAccount("Mike", "mike@isep.ipp.pt", 932755689, null);
+    //account container
+    accountContainer = new AccountContainer();
+    accountContainer.addAccount("Paul", "paul@isep.ipp.pt", 939855689, null);
+    accountContainer.addAccount("Mike", "mike@isep.ipp.pt", 932755689, null);
 
-        //profile container
-        profileContainer = new ProfileContainer();
-        profileContainer.createProfile("Administrator");
-        profileContainer.createProfile("Manager");
+    //profile container
+    profileContainer = new ProfileContainer();
+    profileContainer.createProfile("Administrator");
+    profileContainer.createProfile("Manager");
 
-        //customer
-        customer = new Customer("isep", "222333444");
-        customerContainer = new CustomerContainer();
-        customerContainer.addCustomer(customer.getCustomerName(), customer.getCustomerNif());
-        //project typology
-        projectTypology = new ProjectTypology("Fixed cost");
-        projectTypologyContainer = new ProjectTypologyContainer();
-        projectTypologyContainer.createProjectTypology(projectTypology.getProjectTypologyName());
-        //business sector
-        businessSector = new BusinessSector("fishing");
-        businessSectorContainer = new BusinessSectorContainer();
-        businessSectorContainer.createBusinessSector(businessSector.getBusinessSectorName());
-        //projects
-        projectOne = new Project("AA001", "software development management",
-                customer, projectTypology, businessSector);
-        projectTwo = new Project("AA002", "project software", customer,
-                projectTypology, businessSector);
-        ProjectDtoAsManager projectOneDTO = new ProjectDtoAsManager(projectOne.getProjectCode(),
-                projectOne.getProjectName(), projectOne.getCustomer().getCustomerName(),
-                projectOne.getCustomer().getCustomerNif(),
-                projectOne.getProjectTypology().getProjectTypologyName(),
-                projectOne.getBusinessSector().getBusinessSectorName());
-        ProjectDtoAsManager projectOneTwo = new ProjectDtoAsManager(projectTwo.getProjectCode(),
-                projectTwo.getProjectName(), projectTwo.getCustomer().getCustomerName(),
-                projectTwo.getCustomer().getCustomerNif(),
-                projectTwo.getProjectTypology().getProjectTypologyName(),
-                projectTwo.getBusinessSector().getBusinessSectorName());
-        projectContainer = new ProjectContainer();
-        projectContainer.registerProject(projectOneDTO);
-        projectContainer.registerProject(projectOneTwo);
-        //company
-        company = new Company(accountContainer, profileContainer, null,
-                projectContainer, null, null, null);
-        //controller
-        getListOfProjectscontroller = new GetListOfProjectsController(company);
+    //customer
+    customer = new Customer("isep", "222333444");
+    customerContainer = new CustomerContainer();
+    customerContainer.addCustomer(customer.getCustomerName(), customer.getCustomerNif());
+    //project typology
+    projectTypology = new ProjectTypology("Fixed cost");
+    projectTypologyContainer = new ProjectTypologyContainer();
+    projectTypologyContainer.createProjectTypology(projectTypology.getProjectTypologyName());
+    //business sector
+    businessSector = new BusinessSector("fishing");
+    businessSectorContainer = new BusinessSectorContainer();
+    businessSectorContainer.createBusinessSector(businessSector.getBusinessSectorName());
+    //projects
+    projectOne = new Project("AA001", "software development management",
+            customer, projectTypology, businessSector);
+    projectTwo = new Project("AA002", "project software", customer,
+            projectTypology, businessSector);
+    ProjectDtoAsManager projectOneDTO = new ProjectDtoAsManager(projectOne.getProjectCode(),
+            projectOne.getProjectName(), projectOne.getCustomer().getCustomerName(),
+            projectOne.getCustomer().getCustomerNif(),
+            projectOne.getProjectTypology().getProjectTypologyName(),
+            projectOne.getBusinessSector().getBusinessSectorName());
+    ProjectDtoAsManager projectOneTwo = new ProjectDtoAsManager(projectTwo.getProjectCode(),
+            projectTwo.getProjectName(), projectTwo.getCustomer().getCustomerName(),
+            projectTwo.getCustomer().getCustomerNif(),
+            projectTwo.getProjectTypology().getProjectTypologyName(),
+            projectTwo.getBusinessSector().getBusinessSectorName());
+    projectContainer = new ProjectContainer();
+    projectContainer.registerProject(projectOneDTO, projectTypologyContainer, customerContainer,
+            businessSectorContainer);
+    projectContainer.registerProject(projectOneTwo, projectTypologyContainer, customerContainer,
+            businessSectorContainer);
+    //company
+    company = new Company(accountContainer, profileContainer, null,
+            projectContainer, null, null, null);
+    //controller
+    getListOfProjectscontroller = new GetListOfProjectsController(company);
 
-    }
+  }
 
-    @AfterEach
-    void tearDown() {
-        accountOne = null;
-        accountManager = null;
-        customer = null;
-        projectTypology = null;
-        businessSector = null;
-        projectOne = null;
-        projectTwo = null;
-        accountContainer = null;
-        projectContainer = null;
-        profileContainer = null;
-        company = null;
-        getListOfProjectscontroller = null;
-    }
+  @AfterEach
+  void tearDown() {
+    accountOne = null;
+    accountManager = null;
+    customer = null;
+    projectTypology = null;
+    businessSector = null;
+    projectOne = null;
+    projectTwo = null;
+    accountContainer = null;
+    projectContainer = null;
+    profileContainer = null;
+    company = null;
+    getListOfProjectscontroller = null;
+  }
 
-    /**
-     * Test that returns the list of all projects to an Account with the Manager profile.
-     */
+  /**
+   * Test that returns the list of all projects to an Account with the Manager profile.
+   */
 
-    @Test
-    void ensureThatProjectsIsListedSuccessfully() {
-        // Arrange
-        company.changeProfile("mike@isep.ipp.pt", "Manager");
-        ManagerListProjectsDto projectDTOOne = new ManagerListProjectsDto("AA001", "software " + "development " + "management", "isep", "planned", "fixed cost", "fishing");
-        ManagerListProjectsDto projectDTOTwo = new ManagerListProjectsDto("AA002", "project " + "software",
-                "isep", "planned", "fixed cost", "fishing");
-        List<ManagerListProjectsDto> expectDTOs = new ArrayList<>();
-        expectDTOs.add(projectDTOOne);
-        expectDTOs.add(projectDTOTwo);
-        // Act
-        List<ManagerListProjectsDto> result = getListOfProjectscontroller.getListOfProjects("mike" + "@isep" + ".ipp.pt");
-        // Assert
-        assertEquals(expectDTOs, result);
-    }
+  @Test
+  void ensureThatProjectsIsListedSuccessfully() {
+    // Arrange
+    company.changeProfile("mike@isep.ipp.pt", "Manager");
+    ManagerListProjectsDto projectDTOOne = new ManagerListProjectsDto("AA001", "software " + "development " + "management", "isep", "planned", "fixed cost", "fishing");
+    ManagerListProjectsDto projectDTOTwo = new ManagerListProjectsDto("AA002", "project " + "software",
+            "isep", "planned", "fixed cost", "fishing");
+    List<ManagerListProjectsDto> expectDTOs = new ArrayList<>();
+    expectDTOs.add(projectDTOOne);
+    expectDTOs.add(projectDTOTwo);
+    // Act
+    List<ManagerListProjectsDto> result = getListOfProjectscontroller.getListOfProjects("mike" + "@isep" + ".ipp.pt");
+    // Assert
+    assertEquals(expectDTOs, result);
+  }
 
-    /**
-     * Test that returns an empty list when an unauthorized user access the list of projects.
-     */
-    @Test
-    void ensureThatNoProjectsAreListedWhenRequestedByAnotherProfile() {
-        // Arrange
-        List<ManagerListProjectsDto> expectDTOs = new ArrayList<>();
-        // Act
-        List<ManagerListProjectsDto> result = getListOfProjectscontroller.getListOfProjects("paul" + "@isep" + ".ipp.pt");
-        // Assert
-        assertEquals(expectDTOs, result);
-    }
+  /**
+   * Test that returns an empty list when an unauthorized user access the list of projects.
+   */
+  @Test
+  void ensureThatNoProjectsAreListedWhenRequestedByAnotherProfile() {
+    // Arrange
+    List<ManagerListProjectsDto> expectDTOs = new ArrayList<>();
+    // Act
+    List<ManagerListProjectsDto> result = getListOfProjectscontroller.getListOfProjects("paul" + "@isep" + ".ipp.pt");
+    // Assert
+    assertEquals(expectDTOs, result);
+  }
 
 }
