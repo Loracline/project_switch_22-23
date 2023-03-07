15fc0a9ebea64e7e6709ec98569e97844e66eb73
João Serra [1221842] <1221842@isep.ipp.pt>
2023-03-07 17:17:02 +0000

    US021 : Development : Creation of enum Effort : references #77

---
+++ src/main/java/enums/Effort.java
@@ -0,0 +1,18 @@
+package enums;
+
+public enum Effort {
+
+  ONE(1),
+  TWO(2),
+  THREE(3),
+  FIVE(5),
+  EIGHT(8),
+  THIRTEEN(13),
+  THIRTY_FOUR(34);
+
+  final int effortValue;
+
+  Effort(int effortValue) {
+    this.effortValue = effortValue;
+  }
+}
