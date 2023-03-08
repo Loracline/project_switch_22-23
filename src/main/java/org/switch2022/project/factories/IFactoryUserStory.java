e9749fdb57b6d06aa24cd11fd4884374c6a899a3
Cristiana Moreira [1221826] <1221826@isep.ipp.pt>
2023-03-08 18:16:31 +0000

    US017: implementation: close #42

---
+++ src/main/java/org/switch2022/project/factories/IFactoryUserStory.java
@@ -0,0 +1,9 @@
+package org.switch2022.project.factories;
+
+import org.switch2022.project.model.UserStory;
+
+public class IFactoryUserStory {
+    public UserStory createUserStory(String userStoryNumber, String actor, String userStoryText){
+        return createUserStory(userStoryNumber,actor,userStoryText);
+    }
+}
