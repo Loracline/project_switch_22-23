2aac14da696897f97e20f32d4210716cdf82b740
Mariana Santos [1221848] <1221848@isep.ipp.pt>
2023-03-08 18:27:24 +0000

    US020: Development : refactor to better readability. references #89

--- src/main/java/org/switch2022/project/model/SprintBacklog.java
+++ src/main/java/org/switch2022/project/model/SprintBacklog.java
@@ -2,9 +2,6 @@ package org.switch2022.project.model;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
 /**
@@ -19,41 +16,42 @@ public class SprintBacklog {
 
     private final List<UserStory> userStories = new ArrayList<>();
 
-  /**
-   * Constructor of the class SprintBacklog.
-   */
-  public SprintBacklog() {
-  }
+    /**
+     * Constructor of the class SprintBacklog.
+     */
+    public SprintBacklog() {
+    }
 
-  /**
-   * This method checks if two instances of SprintBacklog are equal by comparing
-   * the userStories.
-   *
-   * @param o SprintBacklog instance to compare with.
-   * @return TRUE if the two have the same attributes, and FALSE otherwise.
-   */
+    /**
+     * This method checks if two instances of SprintBacklog are equal by comparing
+     * the userStories.
+     *
+     * @param o SprintBacklog instance to compare with.
+     * @return TRUE if the two have the same attributes, and FALSE otherwise.
+     */
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (!(o instanceof SprintBacklog)) return false;
-    SprintBacklog that = (SprintBacklog) o;
-    return Objects.equals(userStories, that.userStories);
-  }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {return true;}
+        if (!(o instanceof SprintBacklog)) {return false;}
+        SprintBacklog that = (SprintBacklog) o;
+        return Objects.equals(userStories, that.userStories);
+    }
 
-  /**
-   * The hashCode() method is used to generate a unique hash code for an
-   * object, based on the object's state.
-   *
-   * @return a unique value that represents the object.
-   */
+    /**
+     * The hashCode() method is used to generate a unique hash code for an
+     * object, based on the object's state.
+     *
+     * @return a unique value that represents the object.
+     */
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(userStories);
-  }
+    @Override
+    public int hashCode() {
+        return Objects.hash(userStories);
+    }
 
-    /** This method adds a new User Story to the userStories list is the User Story
+    /**
+     * This method adds a new User Story to the userStories list is the User Story
      * doesn't already exist.
      *
      * @param userStory the new User Story to be added
@@ -70,7 +68,8 @@ public class SprintBacklog {
         return result;
     }
 
-    /** This method verifies if the User Story is already in the list.
+    /**
+     * This method verifies if the User Story is already in the list.
      *
      * @param userStoryNumber to check the presence in the list through the
      *                        hasStoryNumber method.
@@ -79,7 +78,7 @@ public class SprintBacklog {
     public boolean hasUserStory(String userStoryNumber) {
         boolean result = false;
         int i = 0;
-        while (i < userStories.size() && result == false) {
+        while (i < userStories.size() && !result) {
             if (userStories.get(i).hasUserStoryNumber(userStoryNumber)) {
                 result = true;
             }
