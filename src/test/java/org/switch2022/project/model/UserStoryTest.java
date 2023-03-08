388767c2d0b3c52db323537c0bb2dcdc51b51bcd
Cristiana Moreira [1221826] <1221826@isep.ipp.pt>
2023-03-08 19:37:21 +0000

    US017: Implementation: addresses #97

--- src/test/java/org/switch2022/project/model/UserStoryTest.java
+++ src/test/java/org/switch2022/project/model/UserStoryTest.java
@@ -1,10 +1,12 @@
 package org.switch2022.project.model;
 
+
 import org.junit.jupiter.api.Test;
 import org.switch2022.project.utils.Effort;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.switch2022.project.model.UserStory.createUserStory;
 
 class UserStoryTest {
     /*
@@ -17,7 +19,7 @@ class UserStoryTest {
   @Test
   void ensureSameUserStoryEqualsItself() {
     // Arrange
-    UserStory reference = new UserStory("US001", "Manager", "I want to create a profile");
+    UserStory reference = createUserStory("US001", "Manager", "I want to create a profile");
     UserStory other = reference;
     boolean expected = true;
 
@@ -35,8 +37,8 @@ class UserStoryTest {
   @Test
   void ensureTwoUserStoriesAreNotTheSame() {
     // Arrange
-    UserStory reference = new UserStory("US001", "Manager", "I want to create a profile");
-    UserStory other = new UserStory("US002", "Manager", "I want to create a profile");
+    UserStory reference = createUserStory("US001", "Manager", "I want to create a profile");
+    UserStory other = createUserStory("US002", "Manager", "I want to create a profile");
     boolean expected = false;
 
     // Act
@@ -46,93 +48,92 @@ class UserStoryTest {
     assertEquals(expected, result);
   }
 
-  /**
-   * Scenario 3: Verify if a UserStory and a different type of object are not
-   * the same.
-   */
-  @SuppressWarnings("EqualsBetweenInconvertibleTypes")
-  @Test
-  void ensureProfileDoesNotEqualOtherTypeOfObject() {
-    // Arrange
-    UserStory reference = new UserStory("US001", "Manager", "I want to create a profile");
-    String other = "User";
-    boolean expected = false;
+    /**
+     * Scenario 3: Verify if a UserStory and a different type of object are not
+     * the same.
+     */
+    @SuppressWarnings("EqualsBetweenInconvertibleTypes")
+    @Test
+    void ensureUserStoryDoesNotEqualOtherTypeOfObject() {
+        // Arrange
+        UserStory reference = createUserStory("US001", "Manager", "I want to create a profile");
+        String other = "User";
+        boolean expected = false;
 
-    // Act
-    boolean result = reference.equals(other);
+        // Act
+        boolean result = reference.equals(other);
 
-    // Assert
-    assertEquals(expected, result);
-  }
+        // Assert
+        assertEquals(expected, result);
+    }
 
-  /**
-   * Scenario 4: Verify if a UserStory and a null object are not the same.
-   */
-  @Test
-  void ensureProfileDoesNotEqualNull() {
-    // Arrange
-    UserStory reference = new UserStory("US001", "Manager", "I want to create a profile");
-    UserStory other = null;
-    boolean expected = false;
+    /**
+     * Scenario 4: Verify if a UserStory and a null object are not the same.
+     */
+    @Test
+    void ensureUserStoryDoesNotEqualNull() {
+        // Arrange
+        UserStory reference = createUserStory("US001", "Manager", "I want to create a profile");
+        UserStory other = null;
+        boolean expected = false;
 
-    // Act
-    boolean result = reference.equals(other);
+        // Act
+        boolean result = reference.equals(other);
 
-    // Assert
-    assertEquals(expected, result);
-  }
+        // Assert
+        assertEquals(expected, result);
+    }
 
 
     /*
       METHOD hashCode()
      */
 
-  /**
-   * Scenario 1: Two UserStory objects are the same.
-   */
-  @Test
-  public void ensureTwoProfilesHashcodeAreTheSame() {
-    // Arrange
-    UserStory userStoryOne = new UserStory("US001", "Manager", "I want to create a profile");
-    UserStory userStoryTwo = new UserStory("US001", "Manager", "I want to create a profile");
+    /**
+     * Scenario 1: Two UserStory objects are the same.
+     */
+    @Test
+    public void ensureTwoUserStoriesHashcodeAreTheSame() {
+        // Arrange
+        UserStory userStoryOne = createUserStory("US001", "Manager", "I want to create a profile");
+        UserStory userStoryTwo = createUserStory("US001", "Manager", "I want to create a profile");
 
-    // Act
-    int userStoryOneHashCode = userStoryOne.hashCode();
-    int userStoryTwoHashCode = userStoryTwo.hashCode();
+        // Act
+        int userStoryOneHashCode = userStoryOne.hashCode();
+        int userStoryTwoHashCode = userStoryTwo.hashCode();
 
-    // Assert
-    assertEquals(userStoryOneHashCode, userStoryTwoHashCode);
-  }
+        // Assert
+        assertEquals(userStoryOneHashCode, userStoryTwoHashCode);
+    }
 
-  /**
-   * Scenario 2: Two UserStory objects are not the same.
-   */
-  @Test
-  public void ensureTwoProfilesHashcodeAreNotTheSame() {
-    // Arrange
-    UserStory userStoryOne = new UserStory("US001", "Manager", "I want to create a profile");
-    UserStory userStoryThree = new UserStory("US002", "Manager", "I want to create a profile");
+    /**
+     * Scenario 2: Two UserStory objects are not the same.
+     */
+    @Test
+    public void ensureTwoUserStoriesHashcodeAreNotTheSame() {
+        // Arrange
+        UserStory userStoryOne = createUserStory("US001", "Manager", "I want to create a profile");
+        UserStory userStoryThree = createUserStory("US002", "Manager", "I want to create a profile");
 
-    // Act
-    int userStoryOneHashCode = userStoryOne.hashCode();
-    int userStoryThreeHashCode = userStoryThree.hashCode();
+        // Act
+        int userStoryOneHashCode = userStoryOne.hashCode();
+        int userStoryThreeHashCode = userStoryThree.hashCode();
 
-    // Assert
-    assertNotEquals(userStoryOneHashCode, userStoryThreeHashCode);
-  }
+        // Assert
+        assertNotEquals(userStoryOneHashCode, userStoryThreeHashCode);
+    }
 /*
       METHOD hasUserStoryNumber()
      */
-
-  /**
-   * Scenario 1: Verify if userStory is the same by checking its userStoryNumber.
-   * Expected result:True
-   */
+    /**
+     * Scenario 1: Verify if userStory is the same by checking its userStoryNumber.
+     * Expected result:True
+     */
 
   @Test
   public void ensureUserStoryNumberIsTheSame() {
     // Arrange
-    UserStory userStoryOne = new UserStory("US001", "Manager", "I want to create a profile");
+    UserStory userStoryOne = createUserStory("US001", "Manager", "I want to create a profile");
     boolean expected = true;
     //Act
     boolean result = userStoryOne.hasUserStoryNumber("US001");
@@ -147,7 +148,7 @@ class UserStoryTest {
   @Test
   public void ensureUserStoryNumberIsNotTheSame() {
     // Arrange
-    UserStory userStoryOne = new UserStory("US001", "Manager", "I want to create a profile");
+    UserStory userStoryOne = createUserStory("US001", "Manager", "I want to create a profile");
     boolean expected = false;
     //Act
     boolean result = userStoryOne.hasUserStoryNumber("US002");
@@ -167,7 +168,7 @@ class UserStoryTest {
   @Test
   void ensureEffortIsSet() {
     // Arrange
-    UserStory userStoryOne = new UserStory("US001", "Manager",
+    UserStory userStoryOne = createUserStory("US001", "Manager",
             "I want to create a profile");
     // Act
     userStoryOne.setEffort(Effort.TWO);
@@ -184,9 +185,9 @@ class UserStoryTest {
   @Test
   void ensureEffortIsNotTheSameForTwoDifferentUserStories() {
     // Arrange
-    UserStory userStoryOne = new UserStory("US001", "Manager",
+    UserStory userStoryOne = createUserStory("US001", "Manager",
             "I want to create a profile");
-    UserStory userStoryTwo = new UserStory("US002", "Manager",
+    UserStory userStoryTwo = createUserStory("US002", "Manager",
             "I want to create a project");
     // Act
     Effort effortOfUserStoryOne = userStoryOne.getEffort();
