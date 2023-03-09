40bcaafc924460395b520098911e63968abdf15d
João Magalhães [1221840] <1221840@isep.ipp.pt>
2023-03-09 14:05:16 +0000

    US019: implementation: updated Sprint Class, addresses #102

--- src/main/java/org/switch2022/project/model/Sprint.java
+++ src/main/java/org/switch2022/project/model/Sprint.java
@@ -1,46 +1,88 @@
 package org.switch2022.project.model;
 
-import org.switch2022.project.factories.FactoryUserStory;
+
+import org.switch2022.project.factories.FactoryPeriod;
 import org.switch2022.project.utils.Period;
-
 import java.time.LocalDate;
-import java.util.List;
+import java.util.Objects;
 
+
+/**
+ * Represents a short period of time defined by a Sprint Number, Period and Sprint Backlog.
+ */
 class Sprint {
-    private int sprintNumber;
+    private String sprintNumber;
     private FactoryPeriod factoryPeriod;
     private Period period;
+
     private SprintBacklog sprintBacklog;
 
-    public Sprint(LocalDate startDate, int sprintDuration, int sprintNumber) {
-        this.sprintNumber = sprintNumber;
-        this.factoryPeriod = new FactoryPeriod();
-        this.period = new Period(startDate, sprintDuration);
-        this.sprintBacklog = new SprintBacklog();
-    }
-
-    public LocalDate getStartDate() {
-        return period.getStartDate();
-    }
-
-    public LocalDate getEndDate() {
-        return period.getEndDate();
+    /**
+     * Constructor for Sprint class.
+     *
+     * @param startDate      the start date of the sprint.
+     * @param sprintDuration the duration of the sprint in days.
+     * @param sprintNumber   the number of the sprint.
+     */
+    public static Sprint createSprint(LocalDate startDate, int sprintDuration, String sprintNumber, FactoryPeriod factoryPeriod) {
+        Sprint sprint = new Sprint(sprintNumber);
+        sprint.setPeriod(factoryPeriod, sprintDuration,startDate);
+        return sprint;
     }
 
     /**
-     * This method makes a deep copy of the Sprint Backlog of this Sprint.
-     * @param factoryUserStory used to create the copy User Stories.
-     * @return a copy Sprint Backlog with a list of the copied User Stories.
+     * Constructor
+     *
+     * @param sprintNumber the number of the sprint.
      */
-    public SprintBacklog getSprintBacklogCopy(FactoryUserStory factoryUserStory) {
-        SprintBacklog sprintBacklogCopy = new SprintBacklog();
-        List<UserStory> copyList =
-                this.sprintBacklog.getUserStoriesCopy(factoryUserStory);
-        int i = 0;
-        while (i < copyList.size()) {
-            sprintBacklogCopy.addUserStory(copyList.get(i));
-            i++;
-        }
-        return sprintBacklogCopy;
+    private Sprint(String sprintNumber) {this.sprintNumber = sprintNumber.toLowerCase().trim();}
+
+
+    /**
+     * Creates a new period within the sprint.
+     *
+     * @param startDate      the start date of the period
+     * @param periodDuration the duration of the period in weeks
+     */
+    public void createPeriod(LocalDate startDate, int periodDuration) {
+        // create a new period object using the FactoryPeriod class
+        this.period = this.factoryPeriod.createPeriod(startDate, periodDuration);
     }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Sprint sprint = (Sprint) o;
+        return sprintNumber.equals(sprint.sprintNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(sprintNumber);
+    }
+
+    /**
+     * Sets the period for the current instance of the object using the specified factory period,
+     * sprint duration, and start date.
+     * @param factoryPeriod the factory period to use for creating the period
+     * @param sprintDuration the duration of the sprint
+     * @param startDate the start date of the period
+     * */
+
+    private void setPeriod(FactoryPeriod factoryPeriod, int sprintDuration, LocalDate startDate){
+        this.factoryPeriod = factoryPeriod;
+        this.period = factoryPeriod.createPeriod(startDate,sprintDuration);
+    }
+
+
+   /* public isSprintCodeUnique(){
+
+    }
+
+    public isSprintCodeValid() {
+
+
+    }  */
 }
