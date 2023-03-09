61c0d8674d02ca242f11c906140d6697670374d9
Cristiana Moreira [1221826] <1221826@isep.ipp.pt>
2023-03-09 12:16:16 +0000

    US017: implementation: addresses #42

--- src/main/java/org/switch2022/project/factories/FactoryUserStory.java
+++ src/main/java/org/switch2022/project/factories/FactoryUserStory.java
@@ -3,5 +3,5 @@ package org.switch2022.project.factories;
 import org.switch2022.project.model.UserStory;
 
 public interface FactoryUserStory {
-public UserStory createUserStory(String userStoryNumber, String actor, String userStoryText);
+    public UserStory createUserStory(String userStoryNumber, String actor, String userStoryText);
 }
--- src/main/java/org/switch2022/project/factories/IFactoryUserStory.java
+++ src/main/java/org/switch2022/project/factories/IFactoryUserStory.java
@@ -3,7 +3,7 @@ package org.switch2022.project.factories;
 import org.switch2022.project.model.UserStory;
 
 public class IFactoryUserStory {
-    public UserStory createUserStory(String userStoryNumber, String actor, String userStoryText){
-        return createUserStory(userStoryNumber,actor,userStoryText);
+    public UserStory createUserStory(String userStoryNumber, String actor, String userStoryText) {
+        return createUserStory(userStoryNumber, actor, userStoryText);
     }
 }
