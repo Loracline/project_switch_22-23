0fe9583bf8c1f9cd48982202e36bbff90925e4a9
Bárbara Oliveira [1221820] <1221820@isep.ipp.pt>
2023-03-09 12:00:04 +0000

    US022: Update Sprint class | Creation of getSprintBacklog method, addresses #83

--- src/main/java/org/switch2022/project/model/Sprint.java
+++ src/main/java/org/switch2022/project/model/Sprint.java
@@ -1,8 +1,10 @@
 package org.switch2022.project.model;
 
+import org.switch2022.project.factories.FactoryUserStory;
 import org.switch2022.project.utils.Period;
 
 import java.time.LocalDate;
+import java.util.List;
 
 class Sprint {
     private int sprintNumber;
@@ -17,13 +19,28 @@ class Sprint {
         this.sprintBacklog = new SprintBacklog();
     }
 
-
-
     public LocalDate getStartDate() {
         return period.getStartDate();
     }
 
     public LocalDate getEndDate() {
         return period.getEndDate();
+    }
+
+    /**
+     * This method makes a deep copy of the Sprint Backlog of this Sprint.
+     * @param factoryUserStory used to create the copy User Stories.
+     * @return a copy Sprint Backlog with a list of the copied User Stories.
+     */
+    public SprintBacklog getSprintBacklogCopy(FactoryUserStory factoryUserStory) {
+        SprintBacklog sprintBacklogCopy = new SprintBacklog();
+        List<UserStory> copyList =
+                this.sprintBacklog.getUserStoriesCopy(factoryUserStory);
+        int i = 0;
+        while (i < copyList.size()) {
+            sprintBacklogCopy.addUserStory(copyList.get(i));
+            i++;
+        }
+        return sprintBacklogCopy;
     }
 }
