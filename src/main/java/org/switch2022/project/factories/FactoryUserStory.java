b1b16bd0ee43b6c57a4d377ad8250fb301b64825
Cristiana Moreira [1221826] <1221826@isep.ipp.pt>
2023-03-10 11:16:14 +0000

    US017: refactor of class that implements the userStory addresses #107

--- src/main/java/org/switch2022/project/factories/FactoryUserStory.java
+++ src/main/java/org/switch2022/project/factories/FactoryUserStory.java
@@ -2,7 +2,7 @@ package org.switch2022.project.factories;
 
 import org.switch2022.project.model.UserStory;
 
-public class FactoryUserStory {
+public class FactoryUserStory implements  IFactoryUserStory{
     public UserStory createUserStory(String userStoryNumber, String actor, String userStoryText) {
          return UserStory.createUserStory(userStoryNumber, actor, userStoryText);
     }
