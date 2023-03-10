7beb121eda30e7d1f2989801d0a9b2090cd94ff2
João Serra [1221842] <1221842@isep.ipp.pt>
2023-03-10 10:01:50 +0000

    US021 : Development : Refinement of classes Sprint, SprintTest SprintBacklog and SprintBacklogTest, UserStory and UserStoryTest references #84, references #86, references #106

--- src/test/java/org/switch2022/project/model/SprintTest.java
+++ src/test/java/org/switch2022/project/model/SprintTest.java
@@ -8,37 +8,63 @@ import org.switch2022.project.utils.Effort;
 
 import java.time.LocalDate;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.switch2022.project.model.UserStory.createUserStory;
 
 
 class SprintTest {
 
-  /**
-   * METHOD estimateEffortUserStory(userStoryDto, effort)
-   * <p>
-   * Scenario 1: sets the effort of a UserStory.
-   */
+    /**
+     * METHOD estimateEffortUserStory(userStoryDto, effort)
+     * <p>
+     * Scenario 1: sets the effort of a UserStory.
+     */
 
-  @Test
-  void ensureEffortIsIsSetForUserStory() {
-    //Arrange
-    LocalDate date = LocalDate.of(2021, 9, 13);
-    FactoryPeriod factoryPeriod = new IFactoryPeriod();
-    Sprint sprint = Sprint.createSprint(date, 2, "S55", factoryPeriod);
+    @Test
+    void ensureEffortIsSetForUserStory() {
+        //Arrange
+        LocalDate date = LocalDate.of(2021, 9, 13);
+        FactoryPeriod factoryPeriod = new IFactoryPeriod();
+        Sprint sprint = Sprint.createSprint(date, 2, "S55", factoryPeriod);
 
-    UserStory userStory = createUserStory("US001", "Manager",
-            "I want to create a profile");
-    sprint.addUserStoryToSprintBacklog(userStory);
+        UserStory userStory = createUserStory("US001", "Manager",
+                "I want to create a profile");
+        sprint.addUserStoryToSprintBacklog(userStory);
 
-    UserStoryDto userStoryDto = new UserStoryDto("US001", "Manager",
-            "I want to create a profile");
-    Effort effort = Effort.THREE;
+        UserStoryDto userStoryDto = new UserStoryDto("US001", "Manager",
+                "I want to create a profile");
+        Effort effort = Effort.THREE;
 
-    //Act
-    boolean result = sprint.estimateEffortUserStory(userStoryDto, effort);
+        //Act
+        boolean result = sprint.estimateEffortUserStory(userStoryDto, effort);
 
-    //Assert
-    assertTrue(result);
-  }
+        //Assert
+        assertTrue(result);
+    }
+
+    /**
+     * Scenario 2: does not set the effort of a userStory because userStoryNumber does not exist.
+     */
+    @Test
+    void ensureEffortIsNotSetForUserStory() {
+        //Arrange
+        LocalDate date = LocalDate.of(2021, 9, 13);
+        FactoryPeriod factoryPeriod = new IFactoryPeriod();
+        Sprint sprint = Sprint.createSprint(date, 2, "S55", factoryPeriod);
+
+        UserStory userStory = createUserStory("US001", "Manager",
+                "I want to create a profile");
+        sprint.addUserStoryToSprintBacklog(userStory);
+
+        UserStoryDto userStoryDto = new UserStoryDto("US002", "Manager",
+                "I want to create a profile");
+        Effort effort = Effort.THREE;
+
+        //Act
+        boolean result = sprint.estimateEffortUserStory(userStoryDto, effort);
+
+        //Assert
+        assertFalse(result);
+    }
 }
