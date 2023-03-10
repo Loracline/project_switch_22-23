250ef8eb1661b5a54aaf30a33e38205df39c4538
João Magalhães [1221840] <1221840@isep.ipp.pt>
2023-03-10 12:50:16 +0000

    US019: implementation: creation FactorySprint and IFactorySprint , addresses #81

---
+++ src/main/java/org/switch2022/project/factories/FactorySprint.java
@@ -0,0 +1,14 @@
+package org.switch2022.project.factories;
+
+import org.switch2022.project.model.Sprint;
+
+import java.time.LocalDate;
+
+public class FactorySprint {
+
+    public Sprint createSprint(LocalDate startDate, int sprintDuration,
+                               String sprintNumber,
+                               IFactoryPeriod iFactoryPeriod,
+                               IFactorySprintBacklog iFactorySprintBacklog) {
+    return Sprint.createSprint(startDate,sprintDuration,sprintNumber, iFactoryPeriod,iFactorySprintBacklog);}
+}
