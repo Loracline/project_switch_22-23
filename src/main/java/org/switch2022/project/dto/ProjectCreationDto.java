8871079133ce8fdaf2d16357b98837d95970c4fd
Bárbara Oliveira [1221820] <1221820@isep.ipp.pt>
2023-03-14 12:24:31 +0000

    No US: refactor to indent code and correction of typo errors addresses #118

--- src/main/java/org/switch2022/project/dto/ProjectCreationDto.java
+++ src/main/java/org/switch2022/project/dto/ProjectCreationDto.java
@@ -4,56 +4,57 @@ import java.util.Objects;
 
 public class ProjectCreationDto {
 
-  /**
-   * Attributes of the class ProjectDtoAsManager, according to the Class Diagram.
-   */
+    /**
+     * Attributes of the class ProjectDtoAsManager, according to the Class Diagram.
+     */
 
-  public final String code;
-  public final String name;
-  public final String customerName;
-  public final String customerNif;
-  public final String status;
-  public final String projectTypology;
-  public final String businessSector;
+    public final String code;
+    public final String name;
+    public final String customerName;
+    public final String customerNif;
+    public final String status;
+    public final String projectTypology;
+    public final String businessSector;
 
 
-  /**
-   * Constructor of the class ProjectDtoAsManager.
-   */
+    /**
+     * Constructor of the class ProjectDtoAsManager.
+     */
 
-  public ProjectCreationDto(String code, String name, String customerName, String customerNif,
-                            String projectTypology, String businessSector) {
-    this.code = code;
-    this.name = name;
-    this.customerName = customerName.toLowerCase();
-    this.customerNif = customerNif;
-    this.status = "planned";
-    this.projectTypology = projectTypology.toLowerCase();
-    this.businessSector = businessSector.toLowerCase();
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(code, name, customerName, customerNif, status,
-            projectTypology, businessSector);
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
+    public ProjectCreationDto(String code, String name, String customerName,
+                              String customerNif,
+                              String projectTypology, String businessSector) {
+        this.code = code;
+        this.name = name;
+        this.customerName = customerName.toLowerCase();
+        this.customerNif = customerNif;
+        this.status = "planned";
+        this.projectTypology = projectTypology.toLowerCase();
+        this.businessSector = businessSector.toLowerCase();
     }
-    if (o == null) {
-      return false;
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(code, name, customerName, customerNif, status,
+                projectTypology, businessSector);
     }
-    if (o.getClass() != this.getClass()) {
-      return false;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null) {
+            return false;
+        }
+        if (o.getClass() != this.getClass()) {
+            return false;
+        }
+        ProjectCreationDto that = (ProjectCreationDto) o;
+        return Objects.equals(code, that.code) && Objects.equals(name, that.name)
+                && Objects.equals(customerName, that.customerName) &&
+                Objects.equals(customerNif, that.customerNif) &&
+                Objects.equals(projectTypology, that.projectTypology) &&
+                Objects.equals(businessSector, that.businessSector);
     }
-    ProjectCreationDto that = (ProjectCreationDto) o;
-    return Objects.equals(code, that.code) && Objects.equals(name, that.name)
-            && Objects.equals(customerName, that.customerName) &&
-            Objects.equals(customerNif, that.customerNif) &&
-            Objects.equals(projectTypology, that.projectTypology) &&
-            Objects.equals(businessSector, that.businessSector);
-  }
 }
--- src/main/java/org/switch2022/project/factories/FactoryProjectTypology.java
+++ src/main/java/org/switch2022/project/factories/FactoryProjectTypology.java
@@ -3,20 +3,21 @@ package org.switch2022.project.factories;
 import org.switch2022.project.model.ProjectTypology;
 
 /**
- * Class IFactoryProjectTypology implements FactoryProjectTypology ir order to create an object of ProjectTypology.
+ * Class IFactoryProjectTypology implements FactoryProjectTypology ir order to create
+ * an object of ProjectTypology.
  */
 
 public class FactoryProjectTypology implements IFactoryProjectTypology {
 
-  /**
-   * This method creates a projectTypology object.
-   *
-   * @param projectTypologyName is an attribute of projectTypology.
-   * @return a new object ProjectTypology.
-   */
+    /**
+     * This method creates a projectTypology object.
+     *
+     * @param projectTypologyName is an attribute of projectTypology.
+     * @return a new object ProjectTypology.
+     */
 
-  @Override
-  public ProjectTypology createProjectTypology(String projectTypologyName) {
-    return new ProjectTypology(projectTypologyName);
-  }
+    @Override
+    public ProjectTypology createProjectTypology(String projectTypologyName) {
+        return new ProjectTypology(projectTypologyName);
+    }
 }
