a00cf9af3680efb897c9535546deb70e791ddac4
Bárbara Oliveira <1221820@isep.ipp.pt>
2023-04-03 16:29:36 +0100

    US017-US018: Implementation: Added information on Javadoc of classes FactoryUserStory and interface; deleted double validation of createUserStory method. Addresses #158.

--- src/main/java/org/switch2022/project/ddd/domain/model/user_story/IFactoryUserStory.java
+++ src/main/java/org/switch2022/project/ddd/domain/model/user_story/IFactoryUserStory.java
@@ -1,12 +1,13 @@
 package org.switch2022.project.ddd.domain.model.user_story;
 
-import org.switch2022.project.dto.UserStoryCreationDto;
+import org.switch2022.project.ddd.dto.UserStoryCreationDto;
 
 public interface IFactoryUserStory {
     /**
      * This method creates a new User Story based on the provided UserStoryCreationDto.
      *
      * @param userStoryCreationDto the DTO object containing the necessary information to create a User Story.
+     * @param projectCode          the code of the Project where the User Story belongs to.
      * @return the newly created User Story object.
      */
     UserStory createUserStory(UserStoryCreationDto userStoryCreationDto, String projectCode);
