029da5558e40fb4130deae6cdd383efac3c9c8cd
Mariana Santos [1221848] <1221848@isep.ipp.pt>
2023-04-03 08:11:06 +0100

    None US: Development: Added CustomerFactory to AddCustomerController tests which may have disappeared in a commit. references #145

--- src/test/java/org/switch2022/project/controller/AddCustomerControllerTest.java
+++ src/test/java/org/switch2022/project/controller/AddCustomerControllerTest.java
@@ -7,6 +7,8 @@ import org.switch2022.project.container.AccountContainer;
 import org.switch2022.project.container.Company;
 import org.switch2022.project.container.CustomerContainer;
 import org.switch2022.project.container.ProfileContainer;
+import org.switch2022.project.factories.FactoryCustomer;
+import org.switch2022.project.factories.IFactoryCustomer;
 import org.switch2022.project.model.*;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -31,6 +33,7 @@ class AddCustomerControllerTest {
     CustomerContainer customerContainer;
     Company company;
     AddCustomerController addCustomerController;
+    IFactoryCustomer factoryCustomer;
 
     @BeforeEach
     void setUp() {
@@ -51,6 +54,9 @@ class AddCustomerControllerTest {
         projectOne = new Project("1A", "Mobile Software", new Customer("Genius Software", "234567890"),
                 projectTypologyOne, businessSectorOne);
 
+        //factory
+        factoryCustomer = new FactoryCustomer();
+
         //containers
         accountContainer = new AccountContainer();
         accountContainer.addAccount("Mike", "mike@isep.ipp.pt", 932755689, null);
@@ -58,7 +64,7 @@ class AddCustomerControllerTest {
         profileContainer.createProfile("Administrator");
         profileContainer.createProfile("Manager");
         customerContainer = new CustomerContainer();
-        customerContainer.addCustomer("Genius Software", "234567890");
+        customerContainer.addCustomer("Genius Software", "234567890", factoryCustomer);
 
 
         //company
@@ -101,7 +107,8 @@ class AddCustomerControllerTest {
         String emailActor = accountOne.getEmail(); //Administrator
         boolean expected = true;
         //Act
-        boolean result = addCustomerController.addCustomer("Critical", "233444000", emailActor);
+        boolean result = addCustomerController.addCustomer("Critical", "233444000",
+                emailActor, factoryCustomer);
         //Assert
         assertEquals(expected, result);
     }
@@ -120,7 +127,8 @@ class AddCustomerControllerTest {
         String emailActor = accountOne.getEmail(); //Administrator
         boolean expected = false;
         // Act
-        boolean result = addCustomerController.addCustomer("", "234345456", emailActor);
+        boolean result = addCustomerController.addCustomer("", "234345456", emailActor,
+                factoryCustomer);
         // Assert
         assertEquals(expected, result);
     }
@@ -140,7 +148,8 @@ class AddCustomerControllerTest {
         String emailActor = accountOne.getEmail(); //Administrator
         boolean expected = false;
         // Act
-        boolean result = addCustomerController.addCustomer("Critical", "", emailActor);
+        boolean result = addCustomerController.addCustomer("Critical", "", emailActor,
+                factoryCustomer);
         // Assert
         assertEquals(expected, result);
     }
@@ -159,7 +168,8 @@ class AddCustomerControllerTest {
         String emailActor = accountOne.getEmail(); //Administrator
         boolean expected = false;
         // Act
-        boolean result = addCustomerController.addCustomer("Critical", "2223334444", emailActor);
+        boolean result = addCustomerController.addCustomer("Critical", "2223334444",
+                emailActor, factoryCustomer);
         // Assert
         assertEquals(expected, result);
     }
@@ -179,7 +189,8 @@ class AddCustomerControllerTest {
         String emailActor = accountOne.getEmail(); //Manager
         boolean expected = false;
         //Act
-        boolean result = addCustomerController.addCustomer("Critical", "255666777", emailActor);
+        boolean result = addCustomerController.addCustomer("Critical", "255666777",
+                emailActor, factoryCustomer);
         // Assert
         assertEquals(expected, result);
     }
@@ -199,11 +210,11 @@ class AddCustomerControllerTest {
         Company companyDouble = mock(Company.class);
         AddCustomerController addCustomerControllerDouble = new AddCustomerController(companyDouble);
         when(companyDouble.validateProfileRequired(any(), any())).thenReturn(true);
-        when(companyDouble.addCustomer(any(), any())).thenReturn(true);
+        when(companyDouble.addCustomer(any(), any(), any())).thenReturn(true);
 
         //Act
         boolean result = addCustomerControllerDouble.addCustomer("Critical", "233444000",
-                "mike@isep.ipp.pt");
+                "mike@isep.ipp.pt", factoryCustomer);
 
         //Assert
         assertTrue(result);
@@ -219,11 +230,11 @@ class AddCustomerControllerTest {
         Company companyDouble = mock(Company.class);
         AddCustomerController addCustomerControllerDouble = new AddCustomerController(companyDouble);
         when(companyDouble.validateProfileRequired(any(), any())).thenReturn(true);
-        when(companyDouble.addCustomer(any(), any())).thenReturn(false);
+        when(companyDouble.addCustomer(any(), any(), any())).thenReturn(false);
 
         // Act
         boolean result = addCustomerControllerDouble.addCustomer("Mike", "234345456",
-                "mike@isep.ipp.pt");
+                "mike@isep.ipp.pt", factoryCustomer);
 
         // Assert
         assertFalse(result);
@@ -240,11 +251,11 @@ class AddCustomerControllerTest {
         Company companyDouble = mock(Company.class);
         AddCustomerController addCustomerControllerDouble = new AddCustomerController(companyDouble);
         when(companyDouble.validateProfileRequired(any(), any())).thenReturn(false);
-        when(companyDouble.addCustomer(any(), any())).thenReturn(true);
+        when(companyDouble.addCustomer(any(), any(), any())).thenReturn(true);
 
         // Act
         boolean result = addCustomerControllerDouble.addCustomer("Mike", "234345456",
-                "mike@isep.ipp.pt");
+                "mike@isep.ipp.pt", factoryCustomer);
 
         // Assert
         assertFalse(result);
