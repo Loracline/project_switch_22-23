c0533b50a7fe81ecbef301208e436a8200d7b5ed
Caroline Silva [1221823] <1221823@isep.ipp.pt>
2023-04-04 15:26:38 +0100

    No US: Update aIProjectRepository,added getProjectByCode, getProjectNumber and addProjectToProjectRepository methods, addresses #180,

--- src/main/java/org/switch2022/project/ddd/domain/model/project/IProjectRepository.java
+++ src/main/java/org/switch2022/project/ddd/domain/model/project/IProjectRepository.java
@@ -1,5 +1,19 @@
 package org.switch2022.project.ddd.domain.model.project;
 
-public interface IProjectRepository {
+import org.switch2022.project.ddd.domain.value_object.Code;
 
+import java.util.Optional;
+
+/**
+ * Interface for a ProjectRepository.
+ */
+public interface IProjectRepository {
+    /**
+     * Signatures of ProjectRepository class methods to ensure that the interface implements all methods.
+     */
+    Optional<Project> getProjectByCode(Code code);
+
+    int getProjectNumber();
+
+    boolean addProjectToProjectRepository(Project project);
 }
