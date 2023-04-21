32216908ec04e709856e903ca6950e87f2a81806
Bárbara Oliveira <1221820@isep.ipp.pt>
2023-04-21 14:51:03 +0100

    No US: Implementation: Deleted class ProjectDto. Refactored createUs() method of Controller in order to receive as parameter a String instead of Dto. Closes #250.

--- src/main/java/org/switch2022/project/ddd/controller/CreateUserStoryController.java
+++ src/main/java/org/switch2022/project/ddd/controller/CreateUserStoryController.java
@@ -2,15 +2,10 @@ package org.switch2022.project.ddd.controller;
 
 import org.switch2022.project.ddd.application.UsService;
 import org.switch2022.project.ddd.domain.value_object.*;
-import org.switch2022.project.ddd.dto.ProjectDto;
 import org.switch2022.project.ddd.dto.UserStoryCreationDto;
 import org.switch2022.project.ddd.utils.Utils;
 
-import java.util.List;
-
-
-public class CreateUsController {
-
+public class CreateUserStoryController {
     /**
      * The CreateUserStoryController class serves as an intermediary between the user interface (UI) and the
      * business logic of creating a user story and adding it to the product backlog. This controller handles the use
@@ -25,27 +20,25 @@ public class CreateUsController {
     /**
      * Constructor
      */
-    public CreateUsController(UsService usService) {
+    public CreateUserStoryController(UsService usService) {
         this.usService = usService;
     }
 
     /**
      * This method creates a new user story in the requested project and adds it to the product backlog.
      *
-     * @param projectDto           the ProjectDto that represents the project in which the user story will be created
-     * @param userStoryCreationDto the UserStoryCreationDto that represents the data for creating the user story
+     * @param projectCodeOfInterest the code string that represents the project in which the user story will be created
+     * @param userStoryCreationDto  the UserStoryCreationDto that represents the data for creating the user story
      * @return true if the user story is created and added to the product backlog successfully.
      * @throws IllegalArgumentException if either of the input parameters is null.
      * @throws Exception                if an error occurs while creating the user story or adding it to.
      */
-
-
-    public boolean createUs(ProjectDto projectDto, UserStoryCreationDto userStoryCreationDto) throws Exception {
-        if (projectDto == null || userStoryCreationDto == null) {
+    public boolean createUs(String projectCodeOfInterest, UserStoryCreationDto userStoryCreationDto) throws Exception {
+        if (projectCodeOfInterest == null || userStoryCreationDto == null) {
             throw new IllegalArgumentException("Input parameters cannot be null.");
         }
 
-        int codeNumber = Utils.getIntFromAlphanumericString(projectDto.code,"P");
+        int codeNumber = Utils.getIntFromAlphanumericString(projectCodeOfInterest, "P");
         Code projectCode = new Code(codeNumber);
 
         UsNumber userStoryNumber = new UsNumber(userStoryCreationDto.userStoryNumber);
