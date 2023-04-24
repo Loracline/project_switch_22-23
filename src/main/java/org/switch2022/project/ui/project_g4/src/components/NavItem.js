24ec08754b28ffc6b5c03404eb247c3611754d7e
Bárbara Oliveira <1221820@isep.ipp.pt>
2023-04-24 17:47:44 +0100

    No US: Implementation: Created NavItem.js component that represents a Navigation Item in a Menu. Added JSDoc. Closes #256.

---
+++ src/main/java/org/switch2022/project/ui/project_g4/src/components/NavItem.js
@@ -0,0 +1,20 @@
+/**
+ * Represents a navigation item in a menu.
+ * @param item the navigation item.
+ * @param onClick the function to call when the navigation item is clicked.
+ * @returns {JSX.Element} a list item containing a button element with the navigation label.
+ */
+let NavItem;
+NavItem = ({item, onClick}) => {
+    const {key, label} = item;
+
+    return (
+        <li>
+            <button onClick={() => onClick(key)}>
+                {label}
+            </button>
+        </li>
+    )
+};
+
+export default NavItem;
