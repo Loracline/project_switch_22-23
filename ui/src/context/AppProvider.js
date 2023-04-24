fa688ad544dfce7beb1dd68887a50c0202a19615
João Serra [1221842] <1221842@isep.ipp.pt>
2023-04-24 22:14:06 +0100

    Updated AppProvider.js references #261

--- ui/src/context/AppProvider.js
+++ ui/src/context/AppProvider.js
@@ -3,24 +3,35 @@ import PropTypes from 'prop-types';
 import {Provider} from './AppContext.js';
 import appReducer from './Reducer';
 
+// todo dummy data for TableBody component to be deleted
+const data = [
+    {name: 'John Doe', age: 32, gender: 'Male'},
+    {name: 'Jane Doe', age: 28, gender: 'Female'},
+    {name: 'Bob Smith', age: 45, gender: 'Male'},
+    {name: 'Sarah Johnson', age: 24, gender: 'Female'}
+];
+
 /**
  Component that provides the AppContext to its child components using useReducer hook.
 
  @param children - The child components that will have access to AppContext.
  @returns The provider component that wraps the child components.
  */
-const AppProvider = ({children}) =>{
+
+const AppProvider = ({children}) => {
     const initialState = {
         //this is just a dummy initial state, needs to be updated with proper implementation
-        userStories: []
+        userStories: [],
+        data // todo const data for TableBody component to be deleted
     }
+
 
     const [state, dispatch] = useReducer(appReducer, initialState);
 
     return (
         <Provider value={{
             state,
-            dispatch,
+            dispatch
         }}>
             {children}
         </Provider>
