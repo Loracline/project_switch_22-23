185b466de4156f2d9fdfe145350d67923d6402d4
Caroline Silva [1221823] <1221823@isep.ipp.pt>
2023-04-25 17:51:02 +0100

    Sprint#5: Created Table.js in component, closes #264

---
+++ ui/src/components/Table.js
@@ -0,0 +1,21 @@
+import React from "react";
+import TableBody from "./TableBody";
+import TableHeader from "./TableHeader";
+
+/**
+  * Component that allows to create a table.
+  *
+  * 'data' fetched from AppProvider to be inserted in the table.
+  * @returns the table with the data.
+  */
+
+const Table = (props) => {
+  return (
+    <table>
+      <TableHeader headers={props.headers} />
+      <TableBody data={props.data} />
+    </table>
+  );
+};
+
+export default Table;
