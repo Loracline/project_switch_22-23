5944693c77a35fb3c40932b69bf2de716457788a
Sofia Oliveira 1221862 <1221862@isep.ipp.pt>
2023-04-25 22:28:04 +0100

    NO US: Implementation: Created Nav.js component and respective JSDoc, closes #268

---
+++ ui/src/components/Nav.js
@@ -0,0 +1,36 @@
+import React from "react";
+import NavItem from "./NavItem";
+import {selecMenu} from "../context/Actions";
+
+/**
+ * A component that renders a navigation menu with clickable items.
+ *
+ * @param {Object} props - The component props.
+ * @param {Array} props.items - An array of objects representing the menu items to display.
+ * @param {Function} props.dispatch - A function to dispatch actions for state management.
+ * @returns {JSX.Element} A React component that renders the navigation menu.
+ */
+
+const Nav = ({items, dispatch}) => {
+    const onClick = (label) => {
+        const action = selecMenu(label);
+        dispatch(action);
+    }
+    return (
+        <div>
+            <ul>
+                {
+                    items.map(item => (
+                        <NavItem
+                            key={item.key}
+                            item={item}
+                            onClick={onClick}
+                        />
+                    ))
+                }
+            </ul>
+            <hr></hr>
+        </div>
+    )
+}
+export default Nav;
