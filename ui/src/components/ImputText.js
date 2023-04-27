888920c7a3704de14d5bc80ff0ae64411dbc43d3
Rui Pinho <1221860@isep.ipp.pt>
2023-04-27 14:55:17 +0100

    No US: Implementation: Creation of ImputText component. closes #277

---
+++ ui/src/components/ImputText.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+/**
+ * @param title prop is the title of the imput text.
+ * @param contentOfImputText prop is the current text inside the text area.
+ * @param handleTextChange prop is a function that gets called whenever the text area's value changes.
+ * @param htmlFor attribute to associate the label element with the imput element.
+ */
+const ImputText = ({title, handleTextChange, contentOfImputText}) => {
+    return (
+        <>
+            <label htmlFor="myInputText"> {title} </label>
+            <br/>
+            <br/>
+            <input
+                type="text"
+                name="myInputText"
+                id="myInputText"
+                value={contentOfImputText}
+                onChange={handleTextChange}/>
+        </>
+    )
+}
+
+export default ImputText;
