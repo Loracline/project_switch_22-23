528ff565d8a3178435e761ed0853a62123eacb0a
João Serra [1221842] <1221842@isep.ipp.pt>
2023-04-28 10:56:41 +0100

    US 26 : Development : Refactor of ConsultProductBacklog references #286

--- ui/src/pages/ConsultProductBacklog.js
+++ ui/src/pages/ConsultProductBacklog.js
@@ -1,37 +1,42 @@
-import React, {useContext} from 'react';
-import TableBody from "../components/TableBody/TableBody";
-import TableHeader from "../components/TableHeader/TableHeader";
+import React, {useContext, useMemo} from 'react';
+import TableBody from "../components/TableBody";
+import TableHeader from "../components/TableHeader";
 import AppContext from "../context/AppContext";
-import InputText from "../components/InputText";
+import SelectProjectTextInput from "./SelectProjectTextInput";
+import AlertMessages from "../components/AlertMessages";
 
 /**
- * A functional component that displays the product backlog.
- * @returns {JSX.Element} A div element containing a h1 element, an input text component, a table header and
- * a table body element.
+ *  A functional component that displays the product backlog.
+ *  @returns {JSX.Element} A div element containing a h1 element, an input text component, a table header and
+ *  a table body element.
  */
 
 function ConsultProductBacklog() {
-    const {state, dispatch} = useContext(AppContext);
-    const {headers, bodies} = state;
-    let tableData;
-    if (bodies.length > 0) {
-        tableData = (
-            <div>
-                <InputText/>
-                <TableHeader headers={headers}/>
-                <TableBody body={bodies}/>
-            </div>
-        )
-    } else {
-        tableData = <h1>No data.</h1>;
-    }
-
+    const {state} = useContext(AppContext);
+    const {usHeaders, selectedProject} = state;
+    const tableData = useMemo(() => {
+        let tableData;
+        if (selectedProject) {
+            if (selectedProject.userStories.length > 0) {
+                tableData = (
+                    <div>
+                        <TableHeader headers={usHeaders}/>
+                        <TableBody body={selectedProject.userStories}/>
+                    </div>)
+            } else {
+                tableData = <AlertMessages severity="info_userStories"/>;
+            }
+        } else {
+            tableData = <AlertMessages severity="info_projects"/>
+        }
+        return tableData;
+    }, [selectedProject, usHeaders]);
     return (
         <div>
             <h1>Consult Product Backlog</h1>
+            <SelectProjectTextInput/>
             {tableData}
-        </div>
-    );
+        </div>);
 }
 
 export default ConsultProductBacklog;
