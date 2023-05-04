ce2b9ad652890d75af32e601409e6ae1b1e48c54
Bárbara Oliveira <1221820@isep.ipp.pt>
2023-05-04 18:12:54 +0100

    Created CustomerFactory interface and implementation. Addresses #344.

---
+++ src/main/java/org/switch2022/project/ddd/domain/model/customer/CustomerFactory.java
@@ -0,0 +1,21 @@
+package org.switch2022.project.ddd.domain.model.customer;
+
+import org.switch2022.project.ddd.domain.value_object.Name;
+import org.switch2022.project.ddd.domain.value_object.TaxId;
+
+/**
+ * An implementation of the {@link ICustomerFactory} interface that creates new customers.
+ */
+public class CustomerFactory implements ICustomerFactory {
+    /**
+     * Creates a new customer with the specified tax ID, name and phone number.
+     *
+     * @param customerTaxId       the tax ID of the customer.
+     * @param customerName        the name of the customer.
+     * @return a new Customer object with the specified information.
+     */
+    @Override
+    public Customer createCustomer(TaxId customerTaxId, Name customerName) {
+        return new Customer(customerTaxId, customerName);
+    }
+}
