874324deff575472e9071e62e4305006d28b39df
Rui Pinho <1221860@isep.ipp.pt>
2023-05-04 15:34:15 +0100

    NO US : Development : Creation of value object CostPerHour and respective tests. closes #321

---
+++ src/main/java/org/switch2022/project/ddd/domain/value_object/CostPerHour.java
@@ -0,0 +1,61 @@
+package org.switch2022.project.ddd.domain.value_object;
+
+import org.switch2022.project.ddd.domain.shared.ValueObject;
+import org.switch2022.project.ddd.utils.Validate;
+
+import java.util.Objects;
+
+public class CostPerHour implements ValueObject<CostPerHour> {
+    private final float cost;
+
+    /**
+     * Constructor.
+     *
+     * @param cost the id of the Cost Per Hour.
+     */
+    public CostPerHour(final float cost) {
+        Validate.notNegative(cost, "Cost Per Hour");
+        this.cost = cost;
+    }
+
+    /**
+     * This method checks if two instances of CostPerHour are equal by comparing the value of the attribute cost.
+     *
+     * @param other CostPerHour instance to compare with.
+     * @return <code>true</code> if the two instances have the same attribute value, and <code>false</code> otherwise.
+     */
+    @Override
+    public boolean sameValueAs(CostPerHour other) {
+        return Float.compare(this.cost, other.cost) == 0;
+    }
+
+    /**
+     * This method checks if an instance of CostPerHour is equal to another object.
+     *
+     * @param o object to compare with.
+     * @return <code>true</code> if the two instances are equals, and <code>false</code> otherwise.
+     */
+    @Override
+    public boolean equals(Object o) {
+        Validate.notNull(o,"The Object To Compare must not be null");
+        if (this == o) {
+            return true;
+        }
+        if (getClass() != o.getClass()) {
+            return false;
+        }
+        return sameValueAs((CostPerHour) o);
+    }
+
+    /**
+     * This method is used to generate a unique hash code for an object, based on the object's state.
+     *
+     * @return a unique value that represents the object.
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(cost);
+    }
+}
+
+
---
+++ src/test/java/org/switch2022/project/ddd/domain/value_object/CostPerHourTest.java
@@ -0,0 +1,140 @@
+package org.switch2022.project.ddd.domain.value_object;
+
+import org.junit.jupiter.api.Test;
+import org.switch2022.project.ddd.exceptions.InvalidInputException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CostPerHourTest {
+
+    /**
+     * Testing if Cost Per Hour is not created when is equals to zero.
+     */
+    @Test
+    void ensureThatCostPerHourIsCreated_EqualsZero() {
+        // Arrange
+        final float COST_ZERO = 0;
+
+        // Act, Assert
+        assertDoesNotThrow(() -> new CostPerHour(COST_ZERO));
+    }
+
+    /**
+     * Testing if Cost Per Hour is created when is above zero.
+     */
+    @Test
+    void ensureThatCostPerHourIsCreated_Positive() {
+        // Arrange
+        final float COST_POSITIVE = 1;
+
+        // Act, Assert
+        assertDoesNotThrow(() -> new CostPerHour(COST_POSITIVE));
+    }
+
+    /**
+     * Testing if Cost Per Hour is not created when is below zero.
+     */
+    @Test
+    void ensureThatCostPerHourIsNotCreated_BelowZero() {
+        // Arrange
+        final float COST_NEGATIVE = -1;
+
+        // Act, Assert
+        assertThrows(InvalidInputException.class, () -> new CostPerHour(COST_NEGATIVE));
+    }
+
+    /**
+     * Tests for the equals() method.
+     */
+    @Test
+    void ensureThatCostPerHourIsEqualsItself() {
+        // Arrange
+        CostPerHour expected = new CostPerHour(10);
+        CostPerHour result = new CostPerHour(10);
+
+        // Act, Assert
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void ensureThatTwoCostPerHourAreNotEquals() {
+        // Arrange
+        CostPerHour expected = new CostPerHour(10);
+        CostPerHour result = new CostPerHour(11);
+
+        // Assert
+        assertNotEquals(expected, result);
+    }
+
+    @Test
+    @SuppressWarnings("all")
+    void ensureThatCostPerHourIsNotEqualOtherTypeOfObject() {
+        // Arrange
+        String expected = "Hello World!";
+        CostPerHour result = new CostPerHour(11);
+
+        // Act, Assert
+        assertNotEquals(expected, result);
+    }
+
+    /**
+     * Tests for hashCode()
+     */
+    @Test
+    void ensureThatTwoCostPerHourHaveSameHashCode() {
+        // ARRANGE
+        CostPerHour expected = new CostPerHour(11);
+        CostPerHour result = new CostPerHour(11);
+
+        // ACT
+        int hashCodeReference = expected.hashCode();
+        int hashCodeOther = result.hashCode();
+
+        // ASSERT
+        assertEquals(hashCodeOther, hashCodeReference);
+    }
+
+
+    @Test
+    void ensureThatTwoCostPerHourHaveDifferentHashCode() {
+        // ARRANGE
+        CostPerHour expected = new CostPerHour(10);
+        CostPerHour result = new CostPerHour(11);
+
+        // ACT
+        int hashCodeReference = expected.hashCode();
+        int hashCodeOther = result.hashCode();
+
+        // ASSERT
+        assertNotEquals(hashCodeOther, hashCodeReference);
+    }
+
+    /**
+     * Tests for sameValueAs()
+     */
+    @Test
+    void ensureThatTwoCostPerHourHaveTheSameValue() {
+        // Arrange
+        CostPerHour expected = new CostPerHour(11);
+        CostPerHour result = new CostPerHour(11);
+
+        // Act
+        boolean other = expected.sameValueAs(result);
+
+        // Assert
+        assertTrue(other);
+    }
+
+    @Test
+    void ensureThatTwoCostPerHourDoesNotHaveTheSameValue() {
+        // Arrange
+        CostPerHour expected = new CostPerHour(10);
+        CostPerHour result = new CostPerHour(11);
+
+        // Act
+        boolean other = expected.sameValueAs(result);
+
+        // Assert
+        assertFalse(other);
+    }
+}
