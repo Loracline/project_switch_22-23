efa6f778458a4f7d058f3d223f3ac8477af13533
Margarida Ferreira[1221847] <1221847@isep.ipp.pt>
2023-05-05 13:33:27 +0100

    NO US: Implementation: Solved code smells and lack of coverage and formatting issues, addresses #269

--- src/main/java/org/switch2022/project/ddd/domain/model/profile/IProfileFactory.java
+++ src/main/java/org/switch2022/project/ddd/domain/model/profile/IProfileFactory.java
@@ -1,4 +1,5 @@
 package org.switch2022.project.ddd.domain.model.profile;
+
 import org.switch2022.project.ddd.domain.value_object.Name;
 
 /**
@@ -7,14 +8,14 @@ import org.switch2022.project.ddd.domain.value_object.Name;
 
 public interface IProfileFactory {
 
-        /**
-         * This method creates a Profile object with no return.
-         *
-         * @param idProfileNumber of the Profile to create.
-         * @param profileName       is the name.
-         */
+    /**
+     * This method creates a Profile object with no return.
+     *
+     * @param idProfileNumber of the Profile to create.
+     * @param profileName     is the name.
+     */
 
-     Profile createProfile(Name profileName, int idProfileNumber);
-    }
+    Profile createProfile(Name profileName, int idProfileNumber);
+}
 
 
