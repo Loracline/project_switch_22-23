e1eadd747da33d560e9d0be9c1ddb31b273c2ca0
João Serra [1221842] <1221842@isep.ipp.pt>
2023-05-09 21:53:06 +0100

    US 10 : Refactor to DDD : Creation of CreateProjectController.java and CreateProjectControllerTest.java closes #396

---
+++ src/main/java/org/switch2022/project/ddd/controller/CreateProjectController.java
@@ -0,0 +1,26 @@
+package org.switch2022.project.ddd.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.switch2022.project.ddd.application.ProjectCreationService;
+import org.switch2022.project.ddd.dto.ProjectCreationDto;
+
+/**
+ * Class CreateProjectController is built to create business sectors.
+ */
+@Controller
+public class CreateProjectController {
+    @Autowired
+    private ProjectCreationService projectCreationService;
+
+    /**
+     * This method receives a dto as and creates a new project.
+     *
+     * @param projectCreationDto that represents the project to be created.
+     * @return TRUE if the project is created, and throws an AlreadyExistsInRepoException exception otherwise.
+     */
+
+    public String createProject(ProjectCreationDto projectCreationDto) {
+        return projectCreationService.createProject(projectCreationDto);
+    }
+}
