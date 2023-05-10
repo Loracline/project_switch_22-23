d2de86cf95da4212cc145c024bd397687e3ec695
Cristiana Moreira [1221826] <1221826@isep.ipp.pt>
2023-05-10 05:41:41 +0100

    US021: Refactor to DDD: Creation of AddUserStoryToSprintBacklogController closes #402

---
+++ src/main/java/org/switch2022/project/ddd/controller/AddUserStoryToSprintBacklogController.java
@@ -0,0 +1,26 @@
+package org.switch2022.project.ddd.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.switch2022.project.ddd.application.AddUserStoryToSprintBacklogService;
+
+@Controller
+public class AddUserStoryToSprintBacklogController {
+    /**
+     * The AddUserStoryToSprintBacklogController class serves as an intermediary between the user interface
+     * (UI) and the business logic of adding userStories to sprints.
+     * This controller handles the use case specified in the US021 where it is requested to add a user story
+     * to a sprint.
+     */
+    @Autowired
+    AddUserStoryToSprintBacklogService service;
+
+    /**
+     * This method adds a user story to a sprint
+     *
+     * @return true if the user story is added and false if it is already in the sprint.
+     */
+    public boolean addUserStoryToSprintBacklog(String usId, String sprintId) throws Exception {
+        return service.addUserStoryToSprintBacklog(usId, sprintId);
+    }
+}
