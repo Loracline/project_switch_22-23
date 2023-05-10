094998b257bb671ab041d9f893edbb7e8fde282b
Jussara Saraiva Germano de Oliveira [1221845] <1221845@isep.ipp.pt>
2023-05-10 12:39:44 +0100

    US003: Implementation: Refactor to DDD: Creation of ChangeProfileService and respective tests, addresses #405

---
+++ src/main/java/org/switch2022/project/ddd/application/ChangeProfileService.java
@@ -0,0 +1,52 @@
+package org.switch2022.project.ddd.application;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.switch2022.project.ddd.domain.model.account.Account;
+import org.switch2022.project.ddd.domain.model.account.IAccountRepository;
+import org.switch2022.project.ddd.domain.model.profile.IProfileRepository;
+import org.switch2022.project.ddd.domain.model.profile.Profile;
+import org.switch2022.project.ddd.domain.value_object.Name;
+
+
+/**
+ * Class ChangeProfileService acts as an intermediary between the
+ * controller and the business logic underlying the "US003 - As
+ * Administrator, I want to change the profile of a user account."
+ */
+
+@Service
+public class ChangeProfileService {
+    @Autowired
+    private IProfileRepository profileRepository;
+    @Autowired
+    private IAccountRepository accountRepository;
+
+    /**
+     * This method changes the profile of a user account based on the email and the
+     * new profile name provided.
+     *
+     * @param email       the email of the account to change the profile.
+     * @param profileName the name of the new profile to assign to the account.
+     * @return true if the profile was successfully changed, false otherwise.
+     */
+    public boolean changeProfile(String email, String profileName) {
+        boolean wasAccountProfileUpdated = false;
+        Account account = accountRepository.getAccountByEmail(email);
+        Profile profile = profileRepository.getProfileByName(new Name(profileName));
+        if (account != null && profile != null) {
+            account.setProfile(profile);
+            wasAccountProfileUpdated = true;
+        }
+        return wasAccountProfileUpdated;
+    }
+}
+
+
+
+
+
+
+
+
+
---
+++ src/test/java/org/switch2022/project/ddd/application/ChangeProfileServiceTest.java
@@ -0,0 +1,106 @@
+package org.switch2022.project.ddd.application;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.switch2022.project.ddd.domain.model.account.Account;
+import org.switch2022.project.ddd.domain.model.account.AccountFactory;
+import org.switch2022.project.ddd.domain.model.account.IAccountRepository;
+import org.switch2022.project.ddd.domain.model.profile.IProfileRepository;
+import org.switch2022.project.ddd.domain.model.profile.Profile;
+import org.switch2022.project.ddd.domain.model.profile.ProfileFactory;
+import org.switch2022.project.ddd.domain.value_object.Email;
+import org.switch2022.project.ddd.domain.value_object.Name;
+import org.switch2022.project.ddd.domain.value_object.PhoneNumber;
+import org.switch2022.project.ddd.domain.value_object.Photo;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+
+class ChangeProfileServiceTest {
+
+    @InjectMocks
+    private ChangeProfileService changeProfileService;
+    @Mock
+    private IAccountRepository accountRepository;
+    @Mock
+    private IProfileRepository profileRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+
+    /**
+     * Method changeProfile()
+     *
+     * Scenario 1: this test ensures that the account's profile is changed successfully
+     * when given an email and a profile name.
+     * The method should return true, indicating that the profile was successfully changed.
+     * It also verifies that the correct methods of the AccountRepository and ProfileRepository} are called,
+     * and that the returned account has the expected profile set.
+     * Should return true.
+     */
+
+    @Test
+    void ensureThatAccountProfileIsChangedSuccessfully() {
+
+        // Arrange
+        String profileName = "admin";
+        Name name = mock(Name.class);
+        Email email = mock(Email.class);
+        PhoneNumber phoneNumber = mock(PhoneNumber.class);
+        Photo photo = mock(Photo.class);
+        AccountFactory accountFactory = new AccountFactory();
+        Account accountTest = accountFactory.create(name, email, phoneNumber, photo);
+
+        when(accountRepository.getAccountByEmail(String.valueOf(email))).thenReturn(accountTest);
+        ProfileFactory profileFactory = new ProfileFactory();
+        Profile profile = profileFactory.createProfile(name, 1);
+        when(profileRepository.getProfileByName(new Name(profileName))).thenReturn(profile);
+
+        // Act
+        boolean result = changeProfileService.changeProfile(String.valueOf(email), profileName);
+
+        // Assert
+        verify(accountRepository).getAccountByEmail(String.valueOf(email));
+        verify(profileRepository).getProfileByName(new Name(profileName));
+        assertTrue(result);
+        assertEquals(profile, accountTest.getProfile());
+    }
+
+
+    /**
+     *
+     * Scenario 02: this test Attempting to change to an invalid profile.
+     * Should return false and leave the profile unchanged in the account.
+     *
+     */
+    @Test
+    void givenInvalidProfileWhenChangingProfileThenReturnFalse() {
+        // Arrange
+        String profileName = "invalid_profile";
+        Name name = mock(Name.class);
+        Email email = mock(Email.class);
+        PhoneNumber phoneNumber = mock(PhoneNumber.class);
+        Photo photo = mock(Photo.class);
+        AccountFactory accountFactory = new AccountFactory();
+        Account accountTest = accountFactory.create(name, email, phoneNumber, photo);
+
+        when(accountRepository.getAccountByEmail(String.valueOf(email))).thenReturn(accountTest);
+        when(profileRepository.getProfileByName(new Name(profileName))).thenReturn(null);
+
+        // Act
+        boolean result = changeProfileService.changeProfile(String.valueOf(email), profileName);
+
+        // Assert
+        verify(accountRepository).getAccountByEmail(String.valueOf(email));
+        verify(profileRepository).getProfileByName(new Name(profileName));
+        assertFalse(result);
+        assertNull(accountTest.getProfile());
+    }
+}
