0e403bb83a24b18afa28cc0217ae787593c62cdc
Bárbara Oliveira <1221820@isep.ipp.pt>
2023-05-12 09:27:35 +0100

    US009: Refactor to DDD: Created class CreateCustomerController. Tested creation of new customers using dependency injection. Closes #425.

---
+++ src/main/java/org/switch2022/project/ddd/controller/CreateCustomerController.java
@@ -0,0 +1,30 @@
+package org.switch2022.project.ddd.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.switch2022.project.ddd.application.CustomerService;
+
+/**
+ * Controller responsible for creating a new customer.
+ */
+@Controller
+public class CreateCustomerController {
+
+    /**
+     * The customer service used to create the customer.
+     */
+    @SuppressWarnings("all")
+    @Autowired
+    private CustomerService service;
+
+    /**
+     * Creates a new customer with the given tax ID number and name, using the customer service.
+     *
+     * @param taxIdNumber the tax ID number of the customer, which must be valid according to the TaxId validation rules.
+     * @param name        the name of the customer.
+     * @return TRUE if the customer was successfully created, or FALSE otherwise.
+     */
+    public boolean createCustomer(String taxIdNumber, String name) {
+        return service.createCustomer(taxIdNumber, name);
+    }
+}
