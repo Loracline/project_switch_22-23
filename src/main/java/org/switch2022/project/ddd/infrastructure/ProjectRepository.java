4f461fb806d8c6bd6ce7dc121f70071c05805c53
Bárbara Oliveira <1221820@isep.ipp.pt>
2023-05-12 14:04:05 +0100

    US011-US013: Refactor to DDD: ProjectRepository validations on existence of required project and its status being "not closed". Closes #434.

--- src/main/java/org/switch2022/project/ddd/infrastructure/ProjectRepository.java
+++ src/main/java/org/switch2022/project/ddd/infrastructure/ProjectRepository.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Component;
 import org.switch2022.project.ddd.domain.model.project.IProjectRepository;
 import org.switch2022.project.ddd.domain.model.project.Project;
 import org.switch2022.project.ddd.domain.value_object.Code;
+import org.switch2022.project.ddd.domain.value_object.ProjectStatus;
+import org.switch2022.project.ddd.exceptions.InvalidInputException;
+import org.switch2022.project.ddd.exceptions.NotFoundInRepoException;
 
 import java.util.*;
 
@@ -13,6 +16,7 @@ import java.util.*;
  */
 @Component
 public class ProjectRepository implements IProjectRepository {
+
     /**
      * Attributes
      */
@@ -84,7 +88,7 @@ public class ProjectRepository implements IProjectRepository {
      */
     public boolean addProjectToProjectRepository(Project project) {
         boolean projectRegistered = false;
-        if (!doesProjectExist(project)) {
+        if (!projects.contains(project)) {
             projects.add(project);
             projectRegistered = true;
         }
@@ -98,10 +102,24 @@ public class ProjectRepository implements IProjectRepository {
      * @param project existence that shall be verified.
      * @return TRUE if exists, and FALSE otherwise.
      */
-    private boolean doesProjectExist(Project project) {
-        return projects.contains(project);
+    public boolean doesProjectExist(Project project) {
+        if (projects.contains(project)) {
+            return true;
+        } else throw new NotFoundInRepoException("The requested project does not exist.");
     }
 
+    /**
+     * Checks if the given project is not closed.
+     *
+     * @param project the project to check
+     * @return true if the project is not closed, false otherwise
+     * @throws InvalidInputException if the project has already been closed
+     */
+    public boolean isNotClosed(Project project) {
+        if (!project.hasStatus(ProjectStatus.CLOSED)) {
+            return true;
+        } else throw new InvalidInputException("The requested project has already been closed.");
+    }
 
     /**
      * This method returns an unmodifiable view of the projects.
