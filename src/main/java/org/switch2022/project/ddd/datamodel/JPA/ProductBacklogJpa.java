0d537b9c047b6d60c8a8f445fcf5e84f4773f3d6
Cristiana Moreira [1221826] <1221826@isep.ipp.pt>
2023-05-25 09:48:17 +0100

    No US: creation of ProjectJpa aggregate addresses #513

---
+++ src/main/java/org/switch2022/project/ddd/datamodel/JPA/ProductBacklogJpa.java
@@ -0,0 +1,39 @@
+package org.switch2022.project.ddd.datamodel.JPA;
+
+import lombok.Data;
+import lombok.Getter;
+
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "productBacklog")
+public class ProductBacklogJpa {
+    /**
+     * Attributes
+     */
+    @Id
+    @Getter
+    private String productBacklogId;
+    @ElementCollection
+    private List<String> userStories;
+
+    /**
+     * Constructs a new instance of the ProductBacklogJpa class with the provided parameters.
+     *
+     * @param productBacklogId The identifier of the product backlog.
+     * @param userStories      The list of user stories associated with the product backlog.
+     */
+
+    public ProductBacklogJpa(String productBacklogId, List<String> userStories) {
+        this.productBacklogId = productBacklogId;
+        this.userStories = userStories;
+    }
+
+    protected ProductBacklogJpa() {
+    }
+}
