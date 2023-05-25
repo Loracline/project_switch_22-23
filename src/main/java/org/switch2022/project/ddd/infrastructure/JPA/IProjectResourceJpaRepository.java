97d45e142094b45ec2235641f6b91a0b0d819d80
Rui Pinho <1221860@isep.ipp.pt>
2023-05-25 14:58:05 +0100

    US11-13 : Development : Creation of interface that extend the CrudRepository closes #519

---
+++ src/main/java/org/switch2022/project/ddd/infrastructure/JPA/IProjectResourceJpaRepository.java
@@ -0,0 +1,28 @@
+package org.switch2022.project.ddd.infrastructure.JPA;
+
+import org.springframework.data.repository.CrudRepository;
+import org.switch2022.project.ddd.datamodel.JPA.ProjectResourceJpa;
+import org.switch2022.project.ddd.domain.value_object.Code;
+import org.switch2022.project.ddd.domain.value_object.Email;
+
+import java.util.List;
+
+public interface IProjectResourceJpaRepository extends CrudRepository<ProjectResourceJpa, String> {
+
+    /**
+     * This method retrieves a list of ProjectResourceJpa with a given email as attribute.
+     *
+     * @param email the value object email that represents the desired projectResourceJpa.
+     * @return a list of ProjectResourceJpa with a given email as attribute.
+     */
+    List<ProjectResourceJpa> findAllByAccountEmail(Email email);
+
+    /**
+     * This method retrieves a list of ProjectResourceJpa with a given code as attribute.
+     *
+     * @param code the value object code that represents the desired projectResourceJpa.
+     * @return a list of ProjectResourceJpa with a given code as attribute.
+     */
+    List<ProjectResourceJpa> findAllByProjectCode(Code code);
+}
+
