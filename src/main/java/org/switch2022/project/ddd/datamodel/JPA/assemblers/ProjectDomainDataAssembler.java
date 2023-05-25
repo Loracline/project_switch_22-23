7a67daddc4bb878a874077c223fd2410602ca6fd
Cristiana Moreira [1221826] <1221826@isep.ipp.pt>
2023-05-25 09:50:19 +0100

    No US: creation of ProjectDomainDataAssembler and tests addresses #514

---
+++ src/main/java/org/switch2022/project/ddd/datamodel/JPA/assemblers/ProjectDomainDataAssembler.java
@@ -0,0 +1,112 @@
+package org.switch2022.project.ddd.datamodel.JPA.assemblers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.switch2022.project.ddd.datamodel.JPA.ProductBacklogJpa;
+import org.switch2022.project.ddd.datamodel.JPA.ProjectJpa;
+import org.switch2022.project.ddd.domain.model.project.IFactoryProject;
+import org.switch2022.project.ddd.domain.model.project.Project;
+import org.switch2022.project.ddd.domain.value_object.*;
+import org.switch2022.project.ddd.utils.Utils;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
+@Component
+public class ProjectDomainDataAssembler {
+    /**
+     * Converts a Project object to a ProjectJpa object for persistence.
+     *
+     * @param project The Project object to be converted.
+     * @return The converted ProjectJpa object.
+     */
+    @Autowired
+    private IFactoryProject factoryProject;
+
+    public ProjectJpa toData(Project project) {
+
+        Optional<SprintDuration> sprintDurationOptional = project.getSprintDuration();
+        int sprintDuration = 0;
+        if (sprintDurationOptional.isPresent()) {
+            sprintDuration = sprintDurationOptional.get().getSprintDuration();
+        }
+        ProductBacklogJpa productBacklogJpa = createProductBacklogJpa(project);
+
+        return new ProjectJpa(project.getProjectCode(), project.getBudget(), project.getProjectName(),
+                project.getDescription(), project.getProjectStatus(), project.getPlannedSprints(),
+                project.getStartDate(), project.getEndDate(), sprintDuration,
+                project.getBusinessSectorId(), project.getCustomerTaxID(), project.getProjectTypologyId(),
+                productBacklogJpa);
+    }
+
+    /**
+     * Creates a ProductBacklogJpa object based on the provided Project.
+     *
+     * @param project The Project object used to create the ProductBacklogJpa.
+     * @return The created ProductBacklogJpa object.
+     */
+    private ProductBacklogJpa createProductBacklogJpa(Project project) {
+        List<String> userStories = new ArrayList<>();
+        List<UsId> userStoriesId = project.getProductBacklog();
+        for (UsId userStoryId : userStoriesId) {
+            String id = userStoryId.getUserStoryId();
+            userStories.add(id);
+        }
+        return new ProductBacklogJpa(project.getProductBacklogId(), userStories);
+    }
+
+    /**
+     * Converts a ProjectJpa object to a Project domain object.
+     *
+     * @param projectJpa The ProjectJpa object to be converted.
+     * @return The converted Project domain object.
+     */
+
+    public Project toDomain(ProjectJpa projectJpa) {
+        Name projectName = new Name(projectJpa.getProjectName());
+        Description projectDescription = new Description(projectJpa.getDescription());
+        String projectCode = projectJpa.getProjectCode();
+        int projectNumber = Utils.getIntFromAlphanumericString
+                (projectCode, "p");
+        BusinessSectorId businessSectorId = new BusinessSectorId(Utils.getIntFromAlphanumericString
+                (projectJpa.getBusinessSectorId(), "bs"));
+        TaxId taxId = new TaxId(projectJpa.getCustomerTaxId());
+        ProjectTypologyId projectTypology = new ProjectTypologyId(Utils.getIntFromAlphanumericString
+                (projectJpa.getProjectTypologyId(), "pt"));
+        Budget budget = new Budget(BigDecimal.valueOf(projectJpa.getBudget()));
+        ProjectStatus projectstatus = ProjectStatus.valueOf(projectJpa.getProjectStatus());
+        NumberOfPlannedSprints numberOfPlannedSprints = new NumberOfPlannedSprints
+                (projectJpa.getNumberOfPlannedSprints());
+        LocalDate startDate = LocalDate.parse(projectJpa.getStartDate());
+        LocalDate endDate = LocalDate.parse(projectJpa.getEndDate());
+        Period projectPeriod = new Period(startDate, endDate);
+        Project project = factoryProject.createProject(projectNumber, projectName, projectDescription, businessSectorId,
+                taxId, projectTypology);
+        project.setProjectStatus(projectstatus);
+        project.isBudgetAssigned(budget);
+
+        project.isNumberOfPlannedSprintsDefined(numberOfPlannedSprints);
+
+        if (project.hasStatus(ProjectStatus.INCEPTION)) {
+            project.setSprintDuration(projectJpa.getSprintDuration());
+        }
+
+        project.isPeriodAssigned(projectPeriod);
+
+        Iterator<String> userStoriesIterator = projectJpa.getProductBacklog().getUserStories().iterator();
+        int i = 0;
+        while (userStoriesIterator.hasNext()) {
+            String userStoryId = userStoriesIterator.next();
+            String tobeRemoved = projectCode + "_" + "us";
+
+            UsId usid = new UsId(projectCode, Integer.toString(
+                    Utils.getIntFromAlphanumericString(userStoryId, tobeRemoved)));
+            project.addUserStory(i, usid);
+            i++;
+        }
+        return project;
+    }
+}
