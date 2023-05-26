d55630dd833249b2677d0d80de00902383a8451e
Jussara Saraiva Germano de Oliveira [1221845] <1221845@isep.ipp.pt>
2023-05-26 18:59:39 +0100

    US008: Implementation: updated BusinessSectorCreationDto and add tests, closes #544

--- src/main/java/org/switch2022/project/ddd/dto/BusinessSectorCreationDto.java
+++ src/main/java/org/switch2022/project/ddd/dto/BusinessSectorCreationDto.java
@@ -1,5 +1,7 @@
 package org.switch2022.project.ddd.dto;
 
+import java.util.Objects;
+
 /**
  * Represents a sprint creation data transfer object.
  * This class contains information about the sprint such as the project code,
@@ -14,10 +16,49 @@ public class BusinessSectorCreationDto {
 
     /**
      * Constructor to instatiate a new business sector dto.
+     *
      * @param name the name of the
      */
 
-    public BusinessSectorCreationDto (String name) {
+    public BusinessSectorCreationDto(String name) {
         this.name = name;
     }
+
+    /**
+     * The equals() method is used to determine whether two objects are equal in
+     * terms of their content.
+     * The default implementation of the equals() method compares the memory
+     * addresses of the two objects, which means that two objects are considered
+     * equal only if they are the same object in memory.
+     *
+     * @param o accountDto to compare to.
+     * @return TRUE if equal, and FALSE otherwise.
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null) {
+            return false;
+        }
+        if (o.getClass() != this.getClass()) {
+            return false;
+        }
+        BusinessSectorCreationDto that = (BusinessSectorCreationDto) o;
+        return Objects.equals(name, that.name);
+    }
+
+    /**
+     * The hashCode() method is used to generate a unique hash code for an
+     * object, based on the object's state.
+     *
+     * @return a unique value that represents the object.
+     */
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(name);
+    }
 }
+
--- src/test/java/org/switch2022/project/ddd/dto/BusinessSectorCreationDtoTest.java
+++ src/test/java/org/switch2022/project/ddd/dto/BusinessSectorCreationDtoTest.java
@@ -19,4 +19,210 @@ class BusinessSectorCreationDtoTest {
         // Assert
         assertEquals(name, dto.name);
     }
+
+    /**
+     * equals (Object toCompare)
+     * Scenario 1: ensure that a BusinessSectorCreationDto instance is equal to itself.
+     */
+    @Test
+    void ensureSameBusinessSectorCreationDtoEqualsItself() {
+        // ARRANGE
+        BusinessSectorCreationDto reference = new BusinessSectorCreationDto("Technology");
+        BusinessSectorCreationDto other = reference;
+
+        boolean expected = true;
+
+        // ACT
+        boolean result = reference.equals(other);
+
+        // ASSERT
+        assertEquals(expected, result);
+    }
+
+    /**
+     * Scenario 2: ensure that two different BusinessSectorCreationDto instances are not equal.
+     */
+    @Test
+    void ensureTwoBusinessSectorCreationDtoAreNotEqual() {
+        // ARRANGE
+        BusinessSectorCreationDto reference = new BusinessSectorCreationDto("Technology");
+        BusinessSectorCreationDto other = new BusinessSectorCreationDto("Finance");
+
+        boolean expected = false;
+
+        // ACT
+        boolean result = reference.equals(other);
+
+        // ASSERT
+        assertEquals(expected, result);
+    }
+
+    /**
+     * Scenario 3: ensure that a BusinessSectorCreationDto instance is not equal to an object of a different type.
+     */
+    @Test
+    void ensureBusinessSectorCreationDtoNotEqualsOtherTypeObject() {
+        // ARRANGE
+        BusinessSectorCreationDto reference = new BusinessSectorCreationDto("Technology");
+        String other = "User";
+
+        boolean expected = false;
+
+        // ACT
+        boolean result = reference.equals(other);
+
+        // ASSERT
+        assertEquals(expected, result);
+    }
+
+    /**
+     * Scenario 4: case to ensure that a BusinessSectorCreationDto instance is not equal to an object of a different type.
+     */
+    @Test
+    void ensureBusinessSectorCreationDtoNotEqualsDifferentObjectType() {
+        // ARRANGE
+        BusinessSectorCreationDto businessSectorDto = new BusinessSectorCreationDto("Technology");
+        BusinessSectorCreationDto differentObject = null;
+
+        boolean expected = false;
+
+        // ACT
+        boolean result = businessSectorDto.equals(differentObject);
+
+        // ASSERT
+        assertEquals(expected, result);
+    }
+
+    /**
+     * Scenario 5: Test case to ensure that a BusinessSectorCreationDto instance is not equal to null.
+     */
+    @Test
+    void ensureBusinessSectorCreationDtoNotEqualsNull() {
+        // ARRANGE
+        BusinessSectorCreationDto dto = new BusinessSectorCreationDto("Technology");
+
+        // ACT
+        boolean result = dto.equals(null);
+
+        // ASSERT
+        assertFalse(result);
+    }
+
+
+
+    /**
+     * Scenario 6: ensure that two BusinessSectorCreationDto instances with the same name are equal.
+     */
+    @Test
+    void ensureBusinessSectorCreationDtoWithSameNameAreEqual() {
+        // ARRANGE
+        BusinessSectorCreationDto dto1 = new BusinessSectorCreationDto("Technology");
+        BusinessSectorCreationDto dto2 = new BusinessSectorCreationDto("Technology");
+
+        // ACT
+        boolean result = dto1.equals(dto2);
+
+        // ASSERT
+        assertTrue(result);
+    }
+
+    /**
+     * Scenario 7: ensure that two BusinessSectorCreationDto instances with different names are not equal.
+     */
+    @Test
+    void ensureBusinessSectorCreationDtoWithDifferentNamesAreNotEqual() {
+        // ARRANGE
+        BusinessSectorCreationDto dto1 = new BusinessSectorCreationDto("Technology");
+        BusinessSectorCreationDto dto2 = new BusinessSectorCreationDto("Finance");
+
+        // ACT
+        boolean result = dto1.equals(dto2);
+
+        // ASSERT
+        assertFalse(result);
+    }
+
+    /**
+     * Scenario 8: ensure that two BusinessSectorCreationDto instances with empty values are considered equal.
+     */
+    @Test
+    void ensureBusinessSectorCreationDtoWithEmptyValuesAreEqual() {
+        // ARRANGE
+        BusinessSectorCreationDto dto1 = new BusinessSectorCreationDto("");
+        BusinessSectorCreationDto dto2 = new BusinessSectorCreationDto("");
+
+        // ACT
+        boolean result = dto1.equals(dto2);
+
+        // ASSERT
+        assertTrue(result);
+    }
+
+    /**
+     * Scenario 9: ensure that a BusinessSectorCreationDto instance is not equal to an object of a different class.
+     */
+    @Test
+    void ensureBusinessSectorCreationDtoNotEqualsDifferentClassObject() {
+        // ARRANGE
+        BusinessSectorCreationDto dto = new BusinessSectorCreationDto("Technology");
+        Object other = new Object();
+
+        // ACT
+        boolean result = dto.equals(other);
+
+        // ASSERT
+        assertFalse(result);
+    }
+
+    /**
+     * Scenario 10: ensure that a BusinessSectorCreationDto instance is equal to itself.
+     */
+    @Test
+    void ensureBusinessSectorCreationDtoEqualsItself() {
+        // ARRANGE
+        BusinessSectorCreationDto dto = new BusinessSectorCreationDto("Technology");
+
+        // ACT
+        boolean result = dto.equals(dto);
+
+        // ASSERT
+        assertTrue(result);
+    }
+
+
+    /**
+     * hashCode()
+     * Scenario 1: ensure that two BusinessSectorCreationDto instances with the same values have the same hash code.
+     */
+    @Test
+    void ensureBusinessSectorCreationDtoHaveSameHashCode() {
+        // ARRANGE
+        BusinessSectorCreationDto reference = new BusinessSectorCreationDto("Technology");
+        BusinessSectorCreationDto other = new BusinessSectorCreationDto("Technology");
+
+        // ACT
+        int hashCodeReference = reference.hashCode();
+        int hashCodeOther = other.hashCode();
+
+        // ASSERT
+        assertEquals(hashCodeOther, hashCodeReference);
+    }
+
+    /**
+     * Scenario 2: ensure that two BusinessSectorCreationDto instances with different values have different hash codes.
+     */
+    @Test
+    void ensureBusinessSectorCreationDtoHaveDifferentHashCode() {
+        // ARRANGE
+        BusinessSectorCreationDto reference = new BusinessSectorCreationDto("Technology");
+        BusinessSectorCreationDto other = new BusinessSectorCreationDto("Finance");
+
+        // ACT
+        int hashCodeReference = reference.hashCode();
+        int hashCodeOther = other.hashCode();
+
+        // ASSERT
+        assertNotEquals(hashCodeOther, hashCodeReference);
+    }
 }
+
