5309dfef23c63544303c3ee988efce99d7f51dcf
Cristiana Moreira [1221826] <1221826@isep.ipp.pt>
2023-05-29 16:11:39 +0100

    US023: update Service.js addresses #563

--- ui/src/services/Service.js
+++ ui/src/services/Service.js
@@ -4,15 +4,57 @@ export function postSprint(success, failure, sprintToSubmit) {
     fetch(`${URL}/sprints`, {
         method: 'POST',
         body: JSON.stringify({sprintToSubmit}),
-    headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-    },
-})
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
         .then(res => res.text()) // Read response as text)
         .then(res => success(res))
         .catch(err => failure(err.message))
     ;
 }
+
+export function postProject(success, failure, projectToSubmit) {
+    fetch(`${URL}/projects`, {
+        method: 'POST',
+        body: JSON.stringify(projectToSubmit),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+        .then(res => res.json())
+        .then(res => success(res))
+        .catch(err => failure(err.message))
+    ;
+}
+
+export function getBusinessSectors(success, failure) {
+    fetch(`${URL}/business-sectors`, {
+        method: 'GET',
+    })
+        .then(res => res.json())
+        .then(res => success(res))
+        .catch(err => failure(err.message));
+}
+
+export function getProjectTypologies(success, failure) {
+    fetch(`${URL}/typologies`, {
+        method: 'GET',
+    })
+        .then(res => res.json())
+        .then(res => success(res))
+        .catch(err => failure(err.message));
+}
+
+export function getCustomers(success, failure) {
+    fetch(`${URL}/customers`, {
+        method: 'GET',
+    })
+        .then(res => res.json())
+        .then(res => success(res))
+        .catch(err => failure(err.message));
+}
+
 
 export function fetchProject(success, failure, id) {
     fetch(`${URL}/projects?projectCode=${id}`)
