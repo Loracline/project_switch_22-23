e057a6650ae3f3c41ab7f8498d1a4c0ed51845ab
Sofia Oliveira 1221862 <1221862@isep.ipp.pt>
2023-05-29 15:53:17 +0100

    US025: Implementation: Removed unnecessary try-catch statement from createUs method in UserStoryWebController class, and removed respective test, closes #499

--- src/test/java/org/switch2022/project/ddd/webcontrollers/UserStoryWebControllerTest.java
+++ src/test/java/org/switch2022/project/ddd/webcontrollers/UserStoryWebControllerTest.java
@@ -11,7 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.switch2022.project.ddd.application.UsService;
 import org.switch2022.project.ddd.domain.value_object.UsId;
 import org.switch2022.project.ddd.dto.UserStoryCreationDto;
-import org.switch2022.project.ddd.exceptions.AlreadyExistsInRepoException;
 
 import static java.util.Collections.singletonList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -38,7 +37,7 @@ class UserStoryWebControllerTest {
      * @throws Exception if an error occurs during the test execution.
      */
     @Test
-    void ensureThatUserStoryIsCreated() throws Exception {
+    void ensureThatUserStoryIsCreated() {
         //ARRANGE
         UsId usId = mock(UsId.class);
         String acceptanceCriteria = "E-mail must have an @ and a dot";
@@ -55,30 +54,5 @@ class UserStoryWebControllerTest {
         assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
         Object res = responseEntity.getBody();
         assertEquals(res, usId);
-    }
-
-    /**
-     * Scenario 2: Test case to ensure that a user story is not created due to an already
-     * existing user story in the repository.This test verifies the behavior of the createUs
-     * method in the UserStoryWebController class when the user story creation fails due to an
-     * already existing user story.
-     *
-     * @throws AlreadyExistsInRepoException if an error occurs during the test execution.
-     */
-    @Test
-    void ensureThatUserStoryIsNotCreated() throws Exception {
-        String acceptanceCriteria = "E-mail must have an @ and a dot";
-        UserStoryCreationDto userStoryCreationDto = new UserStoryCreationDto("P001", "US001",
-                "As a user, I want to be able to log in to the system.", "User",
-                singletonList(acceptanceCriteria), 1);
-
-        when(this.usService.createUs(userStoryCreationDto)).thenThrow(new
-                AlreadyExistsInRepoException("The User Story is already in the Product Backlog"));
-
-        ResponseEntity<Object> responseEntity =
-                userStoryWebController.createUs(userStoryCreationDto);
-        // Assert
-        assertEquals(HttpStatus.CONFLICT, responseEntity.getStatusCode());
-        assertEquals("The User Story is already in the Product Backlog", responseEntity.getBody());
     }
 }
