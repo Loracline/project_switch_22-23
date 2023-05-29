d49120d3de560dfe5c6268b36d225e1a6c23eb80
Cristiana Moreira [1221826] <1221826@isep.ipp.pt>
2023-05-29 16:11:30 +0100

    US023: update Reducer.js addresses #563

--- ui/src/context/Reducer.js
+++ ui/src/context/Reducer.js
@@ -4,7 +4,22 @@ import {
     CREATE_USER_STORY,
     SELECT_MENU,
     SELECT_PROJECT,
-    CREATE_PROJECT
+    CREATE_PROJECT,
+    CREATE_SPRINT_2,
+    POST_SPRINT_SUCCESS,
+    POST_SPRINT_FAILURE,
+    GET_CUSTOMERS,
+    GET_CUSTOMERS_SUCCESS,
+    GET_CUSTOMERS_FAILURE,
+    GET_BUSINESS_SECTORS,
+    GET_BUSINESS_SECTORS_SUCCESS,
+    GET_BUSINESS_SECTORS_FAILURE,
+    GET_TYPOLOGIES,
+    GET_TYPOLOGIES_SUCCESS,
+    GET_TYPOLOGIES_FAILURE,
+    CREATE_PROJECT_SUCCESS,
+    CREATE_PROJECT_FAILURE,
+    RESET_CREATE_PROJECT
 } from "./Actions";
 
 /** Reducer function that updates the app state based on the dispatched actions.
@@ -17,12 +32,28 @@ const reducer = (state, action) => {
     switch (action.type) {
 
         case CREATE_PROJECT: {
-            const project = action.payload.projectToAdd;
             return {
                 ...state,
-                projects: [...state.projects, project]
+                loading: true
             };
+        }
 
+        case RESET_CREATE_PROJECT: {
+            return {
+                ...state,
+                loading: false,
+                messageSuccess: '',
+                messageFailure: ''
+            }
+        }
+
+        case CREATE_PROJECT_SUCCESS: {
+            const message = action.payload.message;
+            return {...state, loading: false, messageSuccess: message};
+        }
+
+        case CREATE_PROJECT_FAILURE: {
+            return {...state, loading: false, messageFailure: action.payload.error};
         }
 
         case CREATE_USER_STORY: {
@@ -68,6 +99,31 @@ const reducer = (state, action) => {
             const project = action.payload.project;
             return {...state, detailedProject: project};
         }
+
+        case GET_CUSTOMERS:
+        case GET_BUSINESS_SECTORS:
+        case GET_TYPOLOGIES:
+            return {...state, loading: true};
+
+        case GET_CUSTOMERS_SUCCESS: {
+            const customers = action.payload.data;
+            return {...state, loading: false, customers: customers};
+        }
+
+        case GET_BUSINESS_SECTORS_SUCCESS: {
+            const businessSectors = action.payload.data;
+            return {...state, loading: false, businessSectors: businessSectors};
+        }
+
+        case GET_TYPOLOGIES_SUCCESS: {
+            const typologies = action.payload.data;
+            return {...state, loading: false, typologies: typologies};
+        }
+
+        case GET_CUSTOMERS_FAILURE:
+        case GET_BUSINESS_SECTORS_FAILURE:
+        case GET_TYPOLOGIES_FAILURE:
+            return {...state, loading: false, messageFailure: action.payload.error};
 
         /*
         case FETCH_PROJECT_SUCCESS: {
