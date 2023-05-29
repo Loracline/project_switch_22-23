c2b9ec23f28ab9345600a6f2b5d0d0dd42cce693
Caroline Silva [1221823] <1221823@isep.ipp.pt>
2023-05-29 19:49:36 +0100

    US007: Implementation: Updated TypologyWebController, added listAllTypologies method, closes #575

--- src/test/java/org/switch2022/project/ddd/webcontrollers/TypologyWebControllerTest.java
+++ src/test/java/org/switch2022/project/ddd/webcontrollers/TypologyWebControllerTest.java
@@ -6,12 +6,18 @@ import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.switch2022.project.ddd.application.TypologyService;
 import org.switch2022.project.ddd.dto.TypologyCreationDto;
+import org.switch2022.project.ddd.dto.TypologyDto;
 import org.switch2022.project.ddd.exceptions.AlreadyExistsInRepoException;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -60,5 +66,47 @@ class TypologyWebControllerTest {
         ResponseEntity<Object> responseEntity = controller.createTypology(dtoDouble);
         //Assert
         assertEquals(responseEntity.getStatusCodeValue(), 400);
+    }
+    /**
+     * Method: listAllProjects()
+     * Scenario 1: returns a list of TypologiesDto successfully.
+     */
+
+    @Test
+    void ensureThatAListOfProjectsDtoIsReturned() {
+        // Arrange
+        List<TypologyDto> expected = new ArrayList<>();
+        TypologyDto typologyDtoDouble = mock(TypologyDto.class);
+        expected.add(typologyDtoDouble);
+        when(service.requestAllTypologies()).thenReturn(expected);
+
+        // Act
+        ResponseEntity<List<TypologyDto>> responseEntity =
+                controller.listAllTypologies();
+
+        // Assert
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        List<TypologyDto> actual = responseEntity.getBody();
+        assertNotNull(actual);
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * Scenario 2: returns an empty list when there are no typologies.
+     */
+    @Test
+    void ensureThatAnEmptyListIsReturnedWhenThereAreNoProjects() {
+        // Arrange
+        when(service.requestAllTypologies()).thenReturn(Collections.emptyList());
+
+        // Act
+        ResponseEntity<List<TypologyDto>> responseEntity =
+                controller.listAllTypologies();
+
+        // Assert
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        List<TypologyDto> actual = responseEntity.getBody();
+        assertNotNull(actual);
+        assertTrue(actual.isEmpty());
     }
 }
