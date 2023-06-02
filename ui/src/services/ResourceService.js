7469a2ab016c750ad1e182d06da5433ef1f5a32f
Rui Pinho <1221860@isep.ipp.pt>
2023-06-02 13:52:42 +0100

    US028-30 : Development : Updated ResourceService class. closes #615

--- ui/src/services/ResourceService.js
+++ ui/src/services/ResourceService.js
@@ -1,20 +1,14 @@
 import {API_HEADERS as headers, API_ROUTES, API_URL} from './api';
 
-export async function postResource(resourceToAdd) {
-    try {
-        const response = await fetch(`${API_URL}/${API_ROUTES.RESOURCES}`, {
-            method: 'POST',
-            body: JSON.stringify(resourceToAdd),
-            headers,
-        });
-
-        if (response.ok) {
-            return {};
-        } else {
-            const error = await response.json();
-            throw error;
+export function postResource(resourceToAdd) {
+    return fetch(`${API_URL}/${API_ROUTES.RESOURCES}`, {
+        method: 'POST',
+        body: JSON.stringify(resourceToAdd),
+        headers,
+    }).then(async response => {
+        if(!response.ok) {
+            throw await response.json()
         }
-    } catch (error) {
-        throw error;
-    }
+        return {}
+    })
 }
