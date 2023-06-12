9871b7c131faa1c1b6204a7b60c6b7e0a3102cf2
Bárbara Oliveira <1221820@isep.ipp.pt>
2023-06-12 15:04:07 +0100

    US031: Front-End: Implementation: Updated Project page in order to connect to the Sprints list page. Addresses #643.

--- ui/src/pages/Project/Project.js
+++ ui/src/pages/Project/Project.js
@@ -26,14 +26,14 @@ const Project = () => {
                     <div className='buttons-project'>
                         <Button onClick={() => dispatch(selectMenu('createUserStory'))}
                                 text="Create user story" isDisabled={isEndDatePassed || isEndDateNull}/>
-                        <Button onClick={() => dispatch(selectMenu('createSprint'))}
-                                text="Create sprint" isDisabled={!data?.startDate || !data?.endDate || isEndDatePassed}/>
+                        <Button onClick={() => dispatch(selectMenu('sprints'))}
+                                text="Sprints"/>
                         <Button onClick={() => dispatch(selectMenu('productBacklog'))} text="Consult product backlog"/>
                         <Button onClick={() => dispatch(selectMenu('allocateResource'))} text="Add User" isDisabled={!data?.startDate || !data?.endDate || isEndDatePassed}/>
                         <Button onClick={() => dispatch(selectMenu('sprintBacklog'))} text="Sprint Backlog"/>
                     </div>
                 </div>
-                <Button isSecundary={true} onClick={() => dispatch(selectMenu('projects'))} text="Return to projects"/>
+                <Button isSecundary={true} onClick={() => dispatch(selectMenu('projects'))} text="Return"/>
             </section>
         </div>
     )
