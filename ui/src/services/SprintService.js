b89ed1ed28052c7d2ee0ff2c192aac18b86a7eb2
Bárbara Oliveira <1221820@isep.ipp.pt>
2023-06-12 15:15:40 +0100

    US031: Front-End: Implementation: Added function on Sprint Service to fetch sprints from project. Addresses #643.

--- ui/src/services/SprintService.js
+++ ui/src/services/SprintService.js
@@ -15,11 +15,12 @@ export function postSprint(success, failure, sprintToSubmit) {
         }).catch(err => failure(err.message))
     ;
 }
+
 export function postUpdateSprintStatus(sprintId, status) {
     return new Promise((resolve, reject) => {
         fetch(`${URL}/sprints/${sprintId}`, {
             method: 'PUT',
-            body: JSON.stringify({ status }),
+            body: JSON.stringify({status}),
             headers: {
                 'Content-type': 'application/json; charset=UTF-8',
             },
@@ -33,6 +34,24 @@ export function postUpdateSprintStatus(sprintId, status) {
             })
             .catch(err => reject(err.message));
     });
+}
+
+
+/**
+ * Fetches the sprints from a project.
+ * @param success The success callback function that will be called when the request is successful. It receives the
+ * response data as a parameter.
+ * @param failure The failure callback function that will be called when an error occurs. It receives the error message
+ * as a parameter.
+ * @param projectCode The code or identifier of the project to fetch the sprints from.
+ */
+export function fetchSprintsFromProject(success, failure, projectCode) {
+    fetch(
+        `${URL}/sprints/${projectCode}`,
+        {method: 'GET'})
+        .then(res => res.json())
+        .then(res => success(res))
+        .catch(err => failure(err.message));
 }
 
 
