14a8090e14b9eca30bf8ff27a38603d6d6f89e24
Margarida Ferreira[1221847] <1221847@isep.ipp.pt>
2023-06-13 10:51:13 +0100

    US031: Implementation: Added openSprint method to SprintStatusChangeService, updated changeSprintStatus accordingly, and added respective tests. Addresses #661. US034: Implementation: Added test scenarios to closeSprint method in SprintStatusChangeServiceTest to improve coverage. Addresses #660

--- src/main/java/org/switch2022/project/ddd/application/SprintStatusChangeService.java
+++ src/main/java/org/switch2022/project/ddd/application/SprintStatusChangeService.java
@@ -8,8 +8,10 @@ import org.switch2022.project.ddd.domain.model.sprint.Sprint;
 import org.switch2022.project.ddd.domain.model.user_story.IUsRepository;
 import org.switch2022.project.ddd.domain.model.user_story.UserStory;
 import org.switch2022.project.ddd.domain.value_object.SprintId;
+import org.switch2022.project.ddd.domain.value_object.SprintStatus;
 import org.switch2022.project.ddd.domain.value_object.Status;
 import org.switch2022.project.ddd.dto.SprintStatusDto;
+import org.switch2022.project.ddd.exceptions.AlreadyExistsInRepoException;
 import org.switch2022.project.ddd.exceptions.InvalidInputException;
 import org.switch2022.project.ddd.exceptions.NotFoundInRepoException;
 
@@ -31,6 +33,7 @@ public class SprintStatusChangeService {
 
     /**
      * This method changes the status os a sprint.
+     *
      * @param sprintStatusDto with the sprintId and the status.
      * @return true if the status is changed, throws an InvalidInputException if the
      * status is not valid (close or open) and a NotFoundInRepoException when the
@@ -45,8 +48,10 @@ public class SprintStatusChangeService {
         if (sprintOptional.isPresent()) {
             if (sprintStatusDto.getSprintStatus().equalsIgnoreCase("close")) {
                 wasSprintChanged = closeSprint(sprintOptional.get());
+            } else if (sprintStatusDto.getSprintStatus().equalsIgnoreCase("open")) {
+                wasSprintChanged = openSprint(sprintOptional.get());
             } else {
-                throw new InvalidInputException("Sprints can't have the given status");
+                throw new InvalidInputException("Sprint status must be OPEN or CLOSED.");
             }
         } else {
             throw new NotFoundInRepoException("This sprint doesn't exist");
@@ -57,6 +62,7 @@ public class SprintStatusChangeService {
     /**
      * This method closes a sprint and changes the User Stories status that are blocked
      * or running to planned.
+     *
      * @param sprint to be closed.
      * @return true it the sprint is closed, throws an InvalidInputException if the
      * sprint is already closed.
@@ -65,8 +71,8 @@ public class SprintStatusChangeService {
         if (sprint.isOpen()) {
             List<UserStory> userStories =
                     userStoryRepository.getListOfUsWithMatchingIds(sprint.getSprintBacklog());
-            for (UserStory userStory: userStories) {
-                if(isUserStoryBlockedOrRunning(userStory)) {
+            for (UserStory userStory : userStories) {
+                if (isUserStoryBlockedOrRunning(userStory)) {
                     userStory.changeStatus(Status.PLANNED);
                     userStoryRepository.save(userStory);
                 }
@@ -79,11 +85,25 @@ public class SprintStatusChangeService {
 
     /**
      * This method verifies if a User Story has status "running" or "blocked".
+     *
      * @param userStory to be analysed.
      * @return true if the user story has blocked or running status and false otherwise.
      */
     private boolean isUserStoryBlockedOrRunning(UserStory userStory) {
         return userStory.hasStatus(Status.RUNNING) || userStory.hasStatus(Status.BLOCKED);
 
+    }
+
+    /**
+     * This method opens a sprint if there are no other open sprints in the repository.
+     *
+     * @param sprint to be open.
+     * @return true it the sprint is open, throws an AlreadyExistsInRepoException otherwise..
+     */
+    protected boolean openSprint(Sprint sprint) {
+        if (sprintRepository.existsByStatus(SprintStatus.OPEN)) {
+            throw new AlreadyExistsInRepoException("There is currently another OPEN sprint.");
+        }
+        return sprint.changeStatus("OPEN");
     }
 }
